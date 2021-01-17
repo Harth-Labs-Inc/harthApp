3ac6f2ebea0c0af36e5d1b5cd83b3bf32ae7f247
Jorge Piquer <FlGrown@users.noreply.github.com>
2021-01-17 12:53:54 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,23 @@ import { AuthProvider, ProtectRoute } from "../contexts/auth";
 import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
   return (
     <AuthProvider>
       <ProtectRoute>
