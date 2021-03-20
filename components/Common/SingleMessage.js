1b6bc1b3e7393bba3fd5d0c6144a0d7f6dc76339
Jorge Piquer <projectblarg@gmail.com>
2021-03-20 17:55:20 -0400

    style messages

--- components/Common/SingleMessage.js
+++ components/Common/SingleMessage.js
@@ -3,6 +3,32 @@ import React, { useState } from "react";
 const Message = (props) => {
   const { msg } = props;
 
+  let timeStamp;
+  let today = new Date();
+  let weekBefore = today.setDate(today.getDate() - 6);
+
+  if (
+    new Date(msg.date).toLocaleDateString() === new Date().toLocaleDateString()
+  ) {
+    timeStamp = new Date(msg.date).toLocaleTimeString([], {
+      timeStyle: "short",
+    });
+  } else if (new Date(msg.date) >= new Date(weekBefore)) {
+    timeStamp = `${new Date(msg.date).toLocaleDateString("default", {
+      weekday: "long",
+    })} @ ${new Date(msg.date).toLocaleTimeString([], {
+      timeStyle: "short",
+    })}`;
+  } else {
+    timeStamp = `${new Date(msg.date).toLocaleDateString("default", {
+      weekday: "long",
+    })}, ${new Date(msg.date).toLocaleDateString("default", {
+      month: "short",
+    })} @ ${new Date(msg.date).toLocaleTimeString([], {
+      timeStyle: "short",
+    })}`;
+  }
+
   return (
     <div className="message">
       {msg.creator_image ? (
@@ -13,9 +39,8 @@ const Message = (props) => {
       <div>
         <span>
           <p className="message_creator">{msg.creator_name}</p>
-          <p className="message_timestamp">
-            {new Date(msg.date).toLocaleTimeString([], { timeStyle: "short" })}
-          </p>
+
+          <p className="message_timestamp">{timeStamp}</p>
         </span>
         <p className="message_content">{msg.message}</p>
       </div>
--- components/TopicsMenu.js
+++ components/TopicsMenu.js
@@ -4,10 +4,19 @@ import { useComms } from "../contexts/comms";
 const TopicsMenu = (props) => {
   const { selectedTopic } = useComms();
   const { on_toggle_panel } = props;
+  const { on_bookmark } = props;
 
   return (
     <header>
-      {(selectedTopic || {}).title} <p onClick={on_toggle_panel}>menu</p>
+      <p id="topic_title">{(selectedTopic || {}).title}</p>{" "}
+      <span>
+        <button id="topic_bookmark" onClick={on_bookmark}>
+          bookmark
+        </button>
+        <button id="topic_menu_toggle" onClick={on_toggle_panel}>
+          menu
+        </button>
+      </span>
     </header>
   );
 };
