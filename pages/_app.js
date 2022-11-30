3dfd5c01f3f17580c63d87637cde1cedddc98122
Jorge Piquer <piquerjr@gmail.com>
2022-11-30 13:14:47 -0500

    fix app

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,39 @@
-import '../styles/Styles.modules.scss'
-import { AuthProvider, ProtectRoute } from '../contexts/auth'
-import { ResponsiveProvider } from '../contexts/mobile'
-import Layout from '../components/layout'
+import { useEffect } from "react";
+import "../styles/Styles.modules.scss";
+import { AuthProvider, ProtectRoute } from "../contexts/auth";
+import { ResponsiveProvider } from "../contexts/mobile";
+import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(
-          function (registration) {},
-          function (err) {},
-        )
-      })
-    }
-    //disable right click
-    // window.addEventListener(
-    //   "contextmenu",
-    //   function (e) {
-    //     e.preventDefault();
-    //   },
-    //   false
-    // );
-  }, [])
-  return (
-    <ResponsiveProvider>
-      <AuthProvider>
-        <ProtectRoute>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ProtectRoute>
-      </AuthProvider>
-    </ResponsiveProvider>
-  )
+    useEffect(() => {
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", function () {
+                navigator.serviceWorker.register("/sw.js").then(
+                    function (registration) {},
+                    function (err) {}
+                );
+            });
+        }
+        //disable right click
+        // window.addEventListener(
+        //   "contextmenu",
+        //   function (e) {
+        //     e.preventDefault();
+        //   },
+        //   false
+        // );
+    }, []);
+    return (
+        <ResponsiveProvider>
+            <AuthProvider>
+                <ProtectRoute>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </ProtectRoute>
+            </AuthProvider>
+        </ResponsiveProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
