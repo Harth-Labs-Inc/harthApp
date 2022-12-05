7b1ce3c83e99b8ad07738002c71819a779455839
Jorge Piquer <piquerjr@gmail.com>
2022-12-04 19:54:11 -0500

    sdf

--- util/mongodb.js
+++ util/mongodb.js
@@ -3,41 +3,43 @@ import { MongoClient } from "mongodb";
 const { MONGODB_URI, MONGODB_DB } = process.env;
 
 if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
+    throw new Error(
+        "Please define the MONGODB_URI environment variable inside .env.local"
+    );
 }
 
 if (!MONGODB_DB) {
-  throw new Error(
-    "Please define the MONGODB_DB environment variable inside .env.local"
-  );
+    throw new Error(
+        "Please define the MONGODB_DB environment variable inside .env.local"
+    );
 }
 
 let cached = global.mongo;
 
 if (!cached) {
-  cached = global.mongo = { conn: null, promise: null };
+    cached = global.mongo = { conn: null, promise: null };
 }
 
 export async function connectToDatabase() {
-  if (cached.conn) {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        console.log(MONGODB_URI, "MONGODB_URI");
+        cached.promise = MongoClient.connect(MONGODB_URI, opts).then(
+            (client) => {
+                return {
+                    client,
+                    db: client.db(MONGODB_DB),
+                };
+            }
+        );
+    }
+    cached.conn = await cached.promise;
     return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-
-    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
-      return {
-        client,
-        db: client.db(MONGODB_DB),
-      };
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
