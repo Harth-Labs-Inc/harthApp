850144a0df74d72b2a3721d16181789c90a5bdb5
philjackson91 <48493085+philjackson91@users.noreply.github.com>
2022-12-15 15:21:43 -0500

    socket stuff

--- contexts/socket.js
+++ contexts/socket.js
@@ -8,165 +8,134 @@ import { getTopics } from "../requests/community";
 const SocketContext = createContext({});
 
 export const SocketProvider = ({ children }) => {
-    const [socket, setSocket] = useState(null);
-    const [incomingMsg, setIncomingMsg] = useState({});
-    const [incomingMsgUpdate, setIncomingMsgUpdate] = useState({});
-    const [incomingRoomUpdate, setIncomingRoomUpdate] = useState({});
-    const [incomingTopic, setIncomingTopic] = useState({});
-    const [incomingRoom, setIncomingRoom] = useState({});
-    const [unreadMsg, setUnreadMsg] = useState({});
-    const [unreadMsgs, setUnreadMsgs] = useState([]);
+  const [socket, setSocket] = useState(null);
+  const [incomingMsg, setIncomingMsg] = useState({});
+  const [incomingMsgUpdate, setIncomingMsgUpdate] = useState({});
+  const [incomingRoomUpdate, setIncomingRoomUpdate] = useState({});
+  const [incomingTopic, setIncomingTopic] = useState({});
+  const [incomingRoom, setIncomingRoom] = useState({});
+  const [unreadMsg, setUnreadMsg] = useState({});
+  const [unreadMsgs, setUnreadMsgs] = useState([]);
 
-    const { user } = useAuth();
-    const { selectedTopic, setTopics, setSelectedTopic } = useComms();
+  const { user } = useAuth();
+  const { selectedTopic, setTopics, setSelectedTopic, comms } = useComms();
 
-    useEffect(() => {
-        if (user) {
-            let urls = {
-                development: "http://localhost:3030",
-                production: "https://project-blarg-socket.herokuapp.com",
-            };
-            setSocket(
-                io.connect(urls[process.env.NODE_ENV], {
-                    transports: ["websocket"],
-                })
+  useEffect(() => {
+    if (user) {
+      let urls = {
+        development: "http://localhost:3030",
+        production: "https://project-blarg-socket.herokuapp.com",
+      };
+      setSocket(
+        io.connect(urls[process.env.NODE_ENV], {
+          transports: ["websocket"],
+        })
+      );
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (socket && user && comms) {
+      join([...(comms || [])]);
+
+      socket.on("error", function (err) {});
+
+      socket.on("new update", async ({ updateType, ...incomingUpdate }) => {
+        let activeTopic = JSON.parse(localStorage.getItem("selected_topic"));
+        switch (updateType) {
+          case "new message":
+            setIncomingMsg(incomingUpdate);
+            if (incomingUpdate.topic_id !== (selectedTopic || {})._id) {
+              setUnreadMsg(incomingUpdate);
+              setUnreadMsgs([...unreadMsgs, incomingUpdate]);
+            }
+            break;
+          case "message update":
+            setIncomingMsgUpdate(incomingUpdate);
+            break;
+          case "new topic":
+            let newTopicResult = await getTopics(
+              incomingUpdate?.comm_id,
+              user._id
             );
-        }
-    }, [user]);
-
-    useEffect(() => {
-        if (socket && user?.comms) {
-            join([...(user.comms || [])], (err, status) => {
-                let { ok } = status;
-                if (ok) {
-                    console.log("connected");
+            setTopics(newTopicResult.topics);
+            break;
+          case "topic deleted":
+            let deleteResult = getTopics(incomingUpdate?.comm?._id, user._id);
+            deleteResult.then(({ topics }) => {
+              if (topics) {
+                if (activeTopic?._id === incomingUpdate?.topic?._id) {
+                  setSelectedTopic(topics[0]);
                 }
+                setTopics(topics);
+              }
             });
 
-            socket.on("error", function (err) {
-                console.log("received socket error:");
-                console.log(err);
-            });
-
-            socket.on(
-                "new update",
-                async ({ updateType, ...incomingUpdate }) => {
-                    console.log(updateType, incomingUpdate);
-                    let activeTopic = JSON.parse(
-                        localStorage.getItem("selected_topic")
-                    );
-                    switch (updateType) {
-                        case "new message":
-                            setIncomingMsg(incomingUpdate);
-                            if (
-                                incomingUpdate.topic_id !==
-                                (selectedTopic || {})._id
-                            ) {
-                                setUnreadMsg(incomingUpdate);
-                                setUnreadMsgs([...unreadMsgs, incomingUpdate]);
-                            }
-                            break;
-
-                        case "message update":
-                            setIncomingMsgUpdate(incomingUpdate);
-                            break;
-
-                        case "new topic":
-                            let newTopicResult = await getTopics(
-                                incomingUpdate?.comm_id,
-                                user._id
-                            );
-                            setTopics(newTopicResult.topics);
-                            break;
-
-                        case "topic deleted":
-                            console.log(incomingUpdate);
-                            let deleteResult = getTopics(
-                                incomingUpdate?.comm?._id,
-                                user._id
-                            );
-                            deleteResult.then(({ topics }) => {
-                                console.log(topics, "iiiiiiiiii");
-                                if (topics) {
-                                    console.log(
-                                        "deeeeeeeeeeeeeeeeeeeeellllllllllleeeeeeeeeeeeeeeeeeetttttttte",
-                                        deleteResult
-                                    );
-
-                                    console.log(activeTopic, "active topic");
-                                    if (
-                                        activeTopic?._id ===
-                                        incomingUpdate?.topic?._id
-                                    ) {
-                                        setSelectedTopic(topics[0]);
-                                    }
-                                    setTopics(topics);
-                                }
-                            });
-
-                            break;
-                        case "topic edited":
-                            let editResult = await getTopics(
-                                incomingUpdate?.comm?._id,
-                                user._id
-                            );
-
-                            editResult?.topics
-                                ?.filter(Boolean)
-                                .forEach((topic) => {
-                                    if (topic?._id === activeTopic?._id) {
-                                        setSelectedTopic(topic);
-                                    }
-                                });
-                            console.log(editResult, activeTopic);
-                            setTopics(editResult.topics);
-                            break;
-
-                        case "new room":
-                            setIncomingRoom(incomingUpdate);
-                            break;
-
-                        case "room update":
-                            setIncomingRoomUpdate(incomingUpdate);
-                            break;
-
-                        default:
-                            break;
-                    }
-                }
+            break;
+          case "topic edited":
+            let editResult = await getTopics(
+              incomingUpdate?.comm?._id,
+              user._id
             );
+
+            editResult?.topics?.filter(Boolean).forEach((topic) => {
+              if (topic?._id === activeTopic?._id) {
+                setSelectedTopic(topic);
+              }
+            });
+            setTopics(editResult.topics);
+            break;
+          case "new room":
+            setIncomingRoom(incomingUpdate);
+            break;
+          case "room update":
+            setIncomingRoomUpdate(incomingUpdate);
+            break;
+          default:
+            break;
         }
-    }, [socket, user]);
+      });
+    }
+  }, [socket, comms, user]);
 
-    const join = (chatroomName, cb) => {
-        socket.emit("joinRooms", chatroomName, cb);
-    };
-    const leave = (chatroomName, cb) => {
-        socket.emit("leave", chatroomName, cb);
-    };
-    const emitUpdate = (chatroomName, update, cb) => {
-        socket.emit("Update", chatroomName, update, cb);
-    };
+  const join = async (topics) => {
+    let promises = [];
+    for (let { _id } of topics) {
+      promises.push(
+        new Promise((resolve) => {
+          socket.emit("joinRooms", _id, () => resolve(true));
+        })
+      );
+    }
+    await Promise.all(promises);
+    return;
+  };
+  const leave = (chatroomName, cb) => {
+    socket.emit("leave", chatroomName, cb);
+  };
+  const emitUpdate = (chatroomName, update, cb) => {
+    socket.emit("Update", chatroomName, update, cb);
+  };
 
-    return (
-        <SocketContext.Provider
-            value={{
-                emitUpdate,
-                incomingMsgUpdate,
-                incomingTopic,
-                incomingMsg,
-                incomingRoom,
-                incomingRoomUpdate,
-                unreadMsg,
-                unreadMsgs,
-                setUnreadMsgs,
-                join,
-                leave,
-            }}
-        >
-            {children}
-        </SocketContext.Provider>
-    );
+  return (
+    <SocketContext.Provider
+      value={{
+        emitUpdate,
+        incomingMsgUpdate,
+        incomingTopic,
+        incomingMsg,
+        incomingRoom,
+        incomingRoomUpdate,
+        unreadMsg,
+        unreadMsgs,
+        setUnreadMsgs,
+        join,
+        leave,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
 };
 
 export const useSocket = () => useContext(SocketContext);
--- util/mongodb.js
+++ util/mongodb.js
@@ -1,41 +1,74 @@
 import { MongoClient } from "mongodb";
 
-const { MONGODB_URI, MONGODB_DB } = process.env;
+export function connectToDatabase() {
+  const { MONGODB_URI, NODE_ENV } = process.env;
+  //   const uri = process.env.MONGODB_URI;
+  const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  };
 
-if (!MONGODB_URI) {
-    throw new Error(
-        "Please define the MONGODB_URI environment variable inside .env.local"
-    );
-}
+  let client;
+  let clientPromise;
 
-if (!MONGODB_DB) {
-    throw new Error(
-        "Please define the MONGODB_DB environment variable inside .env.local"
-    );
-}
+  console.log(NODE_ENV, "NODE_ENV");
+  if (NODE_ENV === "development") {
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(MONGODB_URI, options);
+      global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+  } else {
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+  }
 
-let cached = global.mongo;
+  return new Promise((resolve) => {
+    async function callConnection() {
+      const db = await clientPromise;
+      resolve({ db: db.db("blarg") });
+    }
+    callConnection();
+  });
+}
+// import { MongoClient } from "mongodb";
 
-if (!cached) {
-    cached = global.mongo = { conn: null, promise: null };
-}
-export async function connectToDatabase() {
-    return new Promise((resolve) => {
-        MongoClient.connect(
-            MONGODB_URI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            },
-            function (err, db) {
-                if (err) {
-                    console.log(err, "mongo err");
-                }
-                resolve({ db: db.db("blarg") });
-            }
-        );
-    });
-}
+// const { MONGODB_URI, MONGODB_DB } = process.env;
+
+// if (!MONGODB_URI) {
+//   throw new Error(
+//     "Please define the MONGODB_URI environment variable inside .env.local"
+//   );
+// }
+
+// if (!MONGODB_DB) {
+//   throw new Error(
+//     "Please define the MONGODB_DB environment variable inside .env.local"
+//   );
+// }
+
+// let cached = global.mongo;
+
+// if (!cached) {
+//   cached = global.mongo = { conn: null, promise: null };
+// }
+// export async function connectToDatabase() {
+//   return new Promise((resolve) => {
+//     MongoClient.connect(
+//       MONGODB_URI,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       },
+//       function (err, db) {
+//         if (err) {
+//           console.log(err, "mongo err");
+//         }
+//         resolve({ db: db.db("blarg") });
+//       }
+//     );
+//   });
+// }
 // export async function connectToDatabase() {
 //     if (cached.conn) {
 //         return cached.conn;
