fa4c5b2571722becb924f36e7ccf775c06c65ecc
Jorge Piquer <piquerjr@gmail.com>
2022-12-22 16:32:38 -0500

    phil/randomstuff

--- components/Menus/TopicsMenu/CreateNewTopicModal/CreateNewTopicModal.js
+++ components/Menus/TopicsMenu/CreateNewTopicModal/CreateNewTopicModal.js
@@ -6,7 +6,7 @@ import { useComms } from "../../../../contexts/comms";
 import { useAuth } from "../../../../contexts/auth";
 import { useSocket } from "../../../../contexts/socket";
 
-import { saveTopics } from "../../../../requests/community";
+import { saveTopics, getHarthByID } from "../../../../requests/community";
 import { addRoomToUsers } from "../../../../requests/rooms";
 
 import styles from "./CreateNewTopicModal.module.scss";
@@ -14,93 +14,97 @@ import { IconBookmarkNoFill } from "../../../../resources/icons/IconBookmarkNoFi
 import { IconTimerNoFill } from "../../../../resources/icons/IconTimerNoFill";
 
 export default function CreateNewTopicModal({ toggleModal }) {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
 
-  const { user } = useAuth();
-  const { selectedcomm } = useComms();
-  const { emitUpdate } = useSocket();
+    const { user } = useAuth();
+    const { selectedcomm } = useComms();
+    const { emitUpdate } = useSocket();
 
-  const watchAge = watch("contentAge", "long");
+    const watchAge = watch("contentAge", "long");
 
-  const createNewTopic = async (data) => {
-    let topic,
-      userIds = [];
+    const createNewTopic = async (data) => {
+        let topic,
+            userIds = [];
+        let result = await getHarthByID(selectedcomm._id);
+        let RecentHarth = result.data;
 
-    selectedcomm.users.forEach((usr) => {
-      userIds.push(usr.userId);
-    });
-    topic = {
-      comm_id: selectedcomm._id,
-      members: [
-        {
-          user_id: user._id,
-          admin: true,
-          muted: false,
-          hidden: false,
-        },
-        ...((selectedcomm || {}).users || []).map((usr) => {
-          if (usr.userId !== user._id) {
-            return {
-              ...usr,
-              user_id: usr.userId,
-              admin: false,
-              muted: false,
-              hidden: false,
-            };
-          }
-        }),
-      ].filter(Boolean),
-      title: data.topicName,
-      invites: [],
-      contentAge: data.contentAge,
-      private: false,
-    };
-    const saveResults = await saveTopics(topic);
-    const { ok, id } = saveResults;
-    if (ok) {
-      topic._id = id;
-      toggleModal();
-      if (id) {
-        const results = await addRoomToUsers(userIds, id);
-        topic.updateType = "new topic";
-        emitUpdate(selectedcomm._id, topic, async (err, status) => {
-          if (err) {
-            console.log(err);
-          }
-          let { ok } = status;
+        RecentHarth.users.forEach((usr) => {
+            userIds.push(usr.userId);
         });
-      }
-    }
-  };
-
-  return (
-    <Modal onToggleModal={toggleModal}>
-      <div className={styles.mainContainer}>
-      <div className={styles.title}>Create a härth</div>
-
-        <form
-          className={styles.CreateTopic}
-          onSubmit={handleSubmit(createNewTopic)}
-        >
-          <input
-            {...register("topicName", { required: true })}
-            placeholder="Topic name"
-            type="text"
-          />
-          <ErrorMessage
-            errorMsg={
-              errors.topicName ? "You must set a Topic name to begin." : null
+        topic = {
+            comm_id: RecentHarth._id,
+            members: [
+                {
+                    user_id: user._id,
+                    admin: true,
+                    muted: false,
+                    hidden: false,
+                },
+                ...((RecentHarth || {}).users || []).map((usr) => {
+                    if (usr.userId !== user._id) {
+                        return {
+                            ...usr,
+                            user_id: usr.userId,
+                            admin: false,
+                            muted: false,
+                            hidden: false,
+                        };
+                    }
+                }),
+            ].filter(Boolean),
+            title: data.topicName,
+            invites: [],
+            contentAge: data.contentAge,
+            private: false,
+        };
+        const saveResults = await saveTopics(topic);
+        const { ok, id } = saveResults;
+        if (ok) {
+            topic._id = id;
+            toggleModal();
+            if (id) {
+                const results = await addRoomToUsers(userIds, id);
+                topic.updateType = "new topic";
+                emitUpdate(RecentHarth._id, topic, async (err, status) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    let { ok } = status;
+                });
             }
-          />
-          <p className={styles.CreateTopicTimeText}>
-            Content in this topic will be kept for 90 days
-          </p>
-          {/* <div className={styles.CreateTopicTime}>
+        }
+    };
+
+    return (
+        <Modal onToggleModal={toggleModal}>
+            <div className={styles.mainContainer}>
+                <div className={styles.title}>Create a härth</div>
+
+                <form
+                    className={styles.CreateTopic}
+                    onSubmit={handleSubmit(createNewTopic)}
+                >
+                    <input
+                        {...register("topicName", { required: true })}
+                        placeholder="Topic name"
+                        type="text"
+                    />
+                    <ErrorMessage
+                        errorMsg={
+                            errors.topicName
+                                ? "You must set a Topic name to begin."
+                                : null
+                        }
+                    />
+                    <p className={styles.CreateTopicTimeText}>
+                        Content in this topic will be kept for 90 days
+                    </p>
+                    {/* <div className={styles.CreateTopicTime}>
             <label
               className={`${styles.CreateTopicTimeButton} ${
                 watchAge === "long" ? styles.Active : null
@@ -135,17 +139,22 @@ export default function CreateNewTopicModal({ toggleModal }) {
           <ErrorMessage
             errorMsg={errors.contentAge ? "You must set contents age" : null}
           /> */}
-          <div className={styles.CreateTopicButtons}>
-            <Button
-              size="large"
-              tier="secondary"
-              text="cancel"
-              onClick={toggleModal}
-            />
-            <Button fullWidth size="large" text="Create Topic" type="submit" />
-          </div>
-        </form>
-      </div>
-    </Modal>
-  );
+                    <div className={styles.CreateTopicButtons}>
+                        <Button
+                            size="large"
+                            tier="secondary"
+                            text="cancel"
+                            onClick={toggleModal}
+                        />
+                        <Button
+                            fullWidth
+                            size="large"
+                            text="Create Topic"
+                            type="submit"
+                        />
+                    </div>
+                </form>
+            </div>
+        </Modal>
+    );
 }
---
+++ pages/api/comm/getHarthByID.js
@@ -0,0 +1,37 @@
+import clientPromise from "../../../util/mongodb";
+
+export default async (req, res) => {
+    let obj;
+    try {
+        obj = JSON.parse(req.body);
+    } catch (e) {
+        obj = req.body;
+    }
+
+    const getComm = (db, id) => {
+        return new Promise((resolve, reject) => {
+            let mongo = require("mongodb");
+            let o_id = new mongo.ObjectID(id);
+            db.collection("communities")
+                .find({ _id: o_id })
+                .toArray(function (err, results) {
+                    if (err) {
+                        resolve(false);
+                    }
+                    if (results[0]) {
+                        resolve(results[0]);
+                    } else {
+                        resolve(false);
+                    }
+                });
+        });
+    };
+
+    const client = await clientPromise;
+    const db = client.db("blarg");
+    let fetchResults = await getComm(db, obj.id);
+    if (!fetchResults) {
+        return res.json({ msg: "Something Went Wrong", ok: 0 });
+    }
+    return res.json({ msg: "successful", ok: 1, data: fetchResults });
+};
