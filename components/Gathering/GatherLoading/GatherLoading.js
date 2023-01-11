ccd94b239d574f763f0d03818416a8c2ebd488bc
Jorge Piquer <piquerjr@gmail.com>
2023-01-11 16:38:16 -0500

    move component

--- components/Gathering/GatherLoading/GatherLoading.js
+++ components/Gathering/GatherLoading/GatherLoading.js
@@ -1,21 +1,20 @@
-import styles from "./gatherLoading.module.scss";
+import styles from "./GatherLoading.module.scss";
 
 export const GatherLoading = () => {
-
     return (
-        <>
-        <div className={styles.gatherLoading} >
+        <div className={styles.gatherLoading}>
             <div className={styles.left} />
 
-            <div className={styles.right} >
+            <div className={styles.right}>
                 <div className={styles.title} />
-                <div className={styles.peopleHolder} >
+                <div className={styles.peopleHolder}>
                     <div className={styles.person} />
                     <div className={styles.person} />
                     <div className={styles.person} />
                 </div>
             </div>
         </div>
-        </>
-    )
+    );
 };
+
+export default GatherLoading;
