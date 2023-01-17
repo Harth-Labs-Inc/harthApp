5c366e49cdd756b92d8a192b75bc5b29f4acdfec
philjackson91 <48493085+philjackson91@users.noreply.github.com>
2023-01-17 13:48:07 -0500

    update logic for tiles and editing

--- components/Gathering/GatheringSchedule/GatheringSchedule.js
+++ components/Gathering/GatheringSchedule/GatheringSchedule.js
@@ -4,7 +4,10 @@ import { convertToAmPm } from "../../../services/helper";
 import { Button, Modal } from "../../Common";
 import { useVideo } from "../../../contexts/video";
 import { saveRoom } from "../../../requests/rooms";
-
+import {
+  updateScheduleRoom,
+  deleteScheduledRoom,
+} from "../../../requests/rooms";
 import { IconHeadsetMic } from "../../../resources/icons/IconHeadsetMic";
 import { IconCastNoFill } from "../../../resources/icons/IconCastNoFill";
 import { IconWorkspace } from "../../../resources/icons/IconWorkspace";
@@ -12,164 +15,166 @@ import styles from "./GatheringSchedule.module.scss";
 import { IconDeleteNoFill } from "../../../resources/icons/IconDeleteNoFill";
 
 const GatheringSchedule = (props) => {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm();
+  let defaults = { defaultValues: {} };
+  if (props.type == "edit" && props.room) {
+    console.log("set defaults");
+    defaults.defaultValues = {
+      ...props.room,
+    };
+  }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm(defaults);
 
-    const { pushScheduledRoom, socketID } = useVideo();
+  const { pushScheduledRoom, socketID, refreshScheduledCallRooms } = useVideo();
 
-    const watchType = watch("gatheringType");
+  const watchType = watch("gatheringType");
 
-    async function scheduleGathering(formData) {
-        let newRoom = { ...formData };
+  async function scheduleGathering(formData) {
+    let newRoom = { ...formData };
 
-        if (newRoom && newRoom.gatheringTime) {
-            newRoom.gatheringTime = convertToAmPm(newRoom.gatheringTime);
-        }
-        newRoom.harthId = props.harthId;
-        newRoom.hostName = props.creator.name;
-        newRoom.icon = props.creator.iconKey || "";
-        newRoom.harthName = props.harthName;
-        newRoom.socketId = socketID;
-        newRoom.acceptedPeers = [
-            { ...props.creator, img: props.creator.iconKey },
-        ];
-
-        const data = await saveRoom(newRoom);
-
-        let { id, ok } = data || {};
-        if (ok) {
-            if (id) {
-                newRoom._id = id;
-                pushScheduledRoom(newRoom);
-                props.closeHandler();
-            }
-        }
+    if (newRoom && newRoom.gatheringTime) {
+      newRoom.gatheringTime = convertToAmPm(newRoom.gatheringTime);
     }
 
-    function handleDelete() {
-        console.log("delete");
+    if (props.type !== "edit") {
+      newRoom.harthId = props.harthId;
+      newRoom.hostName = props.creator.name;
+      newRoom.icon = props.creator.iconKey || "";
+      newRoom.harthName = props.harthName;
+      newRoom.socketId = socketID;
+      newRoom.acceptedPeers = [
+        { ...props.creator, img: props.creator.iconKey },
+      ];
+
+      const data = await saveRoom(newRoom);
+
+      let { id, ok } = data || {};
+      if (ok) {
+        if (id) {
+          newRoom._id = id;
+          pushScheduledRoom(newRoom);
+          props.closeHandler();
+        }
+      }
+    } else {
+      newRoom = { ...props.room, ...newRoom };
+      await updateScheduleRoom(newRoom);
+      refreshScheduledCallRooms(newRoom);
+      props.closeHandler();
     }
+  }
 
-    return (
-        <Modal onToggleModal={props.closeHandler}>
-            <p className={styles.GatheringScheduleTitle}>
-                {props.type === "edit" ? "Edit" : "Schedule a"} gathering
-            </p>
-            <form onSubmit={handleSubmit(scheduleGathering)}>
-                <input
-                    {...register("roomName", { required: true })}
-                    placeholder="[profile name's] party"
-                    type="text"
-                />
-                <ErrorMessage
-                    errorMsg={
-                        errors.gatheringName
-                            ? "Gathering name is required"
-                            : null
-                    }
-                />
-                <p className={styles.GatheringScheduleSubTitle}>Choose date</p>
-                <div className={styles.GatheringScheduleDateTime}>
-                    <input
-                        {...register("gatheringDate", { required: true })}
-                        type="date"
-                    />
-                    <input
-                        {...register("gatheringTime", { required: true })}
-                        type="time"
-                    />
-                </div>
+  async function handleDelete() {
+    console.log("delete");
+    await deleteScheduledRoom(props.room?._id);
+    refreshScheduledCallRooms(props.room);
+    props.closeHandler();
+  }
 
-                <p className={styles.GatheringScheduleSubTitle}>Choose type</p>
-                <div className={styles.GatheringScheduleType}>
-                    <label
-                        className={`${styles.GatheringScheduleTypeSelect} ${
-                            watchType === "voice" ? styles.Active : null
-                        }`}
-                    >
-                        <span>
-                            <IconHeadsetMic
-                                fill={
-                                    watchType === "voice" ? "#fff" : "#2F1D2A80"
-                                }
-                            />
-                        </span>
-                        Voice
-                        <input
-                            {...register("gatheringType", { required: true })}
-                            type="radio"
-                            value="voice"
-                        />
-                    </label>
-                    <label
-                        className={`${styles.GatheringScheduleTypeSelect} ${
-                            watchType === "stream" ? styles.Active : null
-                        }`}
-                    >
-                        <span>
-                            <IconCastNoFill
-                                fill={
-                                    watchType === "stream"
-                                        ? "#fff"
-                                        : "#2F1D2A80"
-                                }
-                            />
-                        </span>
-                        Stream
-                        <input
-                            {...register("gatheringType", { required: true })}
-                            type="radio"
-                            value="stream"
-                        />
-                    </label>
-                    <label
-                        className={`${styles.GatheringScheduleTypeSelect} ${
-                            watchType === "party" ? styles.Active : null
-                        }`}
-                    >
-                        <span>
-                            <IconWorkspace
-                                fill={
-                                    watchType === "party" ? "#fff" : "#2F1D2A80"
-                                }
-                            />
-                        </span>
-                        Party
-                        <input
-                            {...register("gatheringType", { required: true })}
-                            type="radio"
-                            value="party"
-                        />
-                    </label>
-                </div>
+  return (
+    <Modal onToggleModal={props.closeHandler}>
+      <p className={styles.GatheringScheduleTitle}>
+        {props.type === "edit" ? "Edit" : "Schedule a"} gathering
+      </p>
+      <form onSubmit={handleSubmit(scheduleGathering)}>
+        <input
+          {...register("roomName", { required: true })}
+          placeholder="[profile name's] party"
+          type="text"
+        />
+        <ErrorMessage
+          errorMsg={errors.gatheringName ? "Gathering name is required" : null}
+        />
+        <p className={styles.GatheringScheduleSubTitle}>Choose date</p>
+        <div className={styles.GatheringScheduleDateTime}>
+          <input
+            {...register("gatheringDate", { required: true })}
+            type="date"
+          />
+          <input
+            {...register("gatheringTime", { required: true })}
+            type="time"
+          />
+        </div>
 
-                <p className={styles.GatheringScheduleSubTitle}>
-                    Give a description (optional)
-                </p>
-                <input
-                    {...register("gatheringDescription")}
-                    placeholder="let's all..."
-                />
-                <div className={styles.GatheringScheduleButtons}>
-                    {props.type === "edit" ? (
-                        <button onClick={handleDelete}>
-                            <IconDeleteNoFill />
-                        </button>
-                    ) : null}
-                    <Button
-                        tier="secondary"
-                        text="Cancel"
-                        onClick={props.closeHandler}
-                    />
-                    <Button fullWidth type="Submit" text="Schedule" />
-                </div>
-            </form>
-        </Modal>
-    );
+        <p className={styles.GatheringScheduleSubTitle}>Choose type</p>
+        <div className={styles.GatheringScheduleType}>
+          <label
+            className={`${styles.GatheringScheduleTypeSelect} ${
+              watchType === "voice" ? styles.Active : null
+            }`}
+          >
+            <span>
+              <IconHeadsetMic
+                fill={watchType === "voice" ? "#fff" : "#2F1D2A80"}
+              />
+            </span>
+            Voice
+            <input
+              {...register("gatheringType", { required: true })}
+              type="radio"
+              value="voice"
+            />
+          </label>
+          <label
+            className={`${styles.GatheringScheduleTypeSelect} ${
+              watchType === "stream" ? styles.Active : null
+            }`}
+          >
+            <span>
+              <IconCastNoFill
+                fill={watchType === "stream" ? "#fff" : "#2F1D2A80"}
+              />
+            </span>
+            Stream
+            <input
+              {...register("gatheringType", { required: true })}
+              type="radio"
+              value="stream"
+            />
+          </label>
+          <label
+            className={`${styles.GatheringScheduleTypeSelect} ${
+              watchType === "party" ? styles.Active : null
+            }`}
+          >
+            <span>
+              <IconWorkspace
+                fill={watchType === "party" ? "#fff" : "#2F1D2A80"}
+              />
+            </span>
+            Party
+            <input
+              {...register("gatheringType", { required: true })}
+              type="radio"
+              value="party"
+            />
+          </label>
+        </div>
+
+        <p className={styles.GatheringScheduleSubTitle}>
+          Give a description (optional)
+        </p>
+        <input
+          {...register("gatheringDescription")}
+          placeholder="let's all..."
+        />
+        <div className={styles.GatheringScheduleButtons}>
+          {props.type === "edit" ? (
+            <button onClick={handleDelete}>
+              <IconDeleteNoFill />
+            </button>
+          ) : null}
+          <Button tier="secondary" text="Cancel" onClick={props.closeHandler} />
+          <Button fullWidth type="Submit" text="Schedule" />
+        </div>
+      </form>
+    </Modal>
+  );
 };
 
 export default GatheringSchedule;
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -6,7 +6,7 @@ import { ChatProvider } from "../../contexts/chat";
 import { useAuth } from "../../contexts/auth";
 import { sendWelcomeEmailToUser } from "../../requests/userApi";
 import Cookies from "js-cookie";
-
+import { checkIfInviteTokenIsGood } from "../../requests/community";
 import { VideoProvider } from "../../contexts/video";
 import DashboardLayout from "../../components/DashboardLayout/DashboardLayout";
 
@@ -22,221 +22,199 @@ import CreateHarthProfile from "../../components/createHarthProfile/createHarthP
 import HarthInviteAcceptModal from "../../components/harthInviteAcceptModal/harthInviteAcceptModal";
 
 const dashboard = (props) => {
-    const [currentPage, setCurrentPage] = useState();
-    const [GatherWindow, setGatherWindow] = useState("");
+  const [currentPage, setCurrentPage] = useState();
+  const [GatherWindow, setGatherWindow] = useState("");
 
-    const [invitedHarth, setInvitedHarth] = useState(null);
-    const [newHarth, setNewHarth] = useState(null);
+  const [invitedHarth, setInvitedHarth] = useState(null);
+  const [newHarth, setNewHarth] = useState(null);
 
-    const [showCreateHarthNameModal, setShowCreateHarthNameModal] =
-        useState(false);
-    const [showCreateHarthProfileModal, setShowCreateHarthProfileModal] =
-        useState(false);
+  const [showCreateHarthNameModal, setShowCreateHarthNameModal] =
+    useState(false);
+  const [showCreateHarthProfileModal, setShowCreateHarthProfileModal] =
+    useState(false);
 
-    const [showInviteAcceptModal, setShowInviteAcceptModal] = useState(false);
-    const [showInviteProfileModal, setShowInviteProfileModal] = useState(false);
+  const [showInviteAcceptModal, setShowInviteAcceptModal] = useState(false);
+  const [showInviteProfileModal, setShowInviteProfileModal] = useState(false);
 
-    const { user, loading, inviteTKN } = useAuth();
+  const { user, loading, inviteTKN } = useAuth();
 
-    const router = useRouter();
-    const {
-        query: { tkn },
-    } = router;
+  const router = useRouter();
+  const {
+    query: { tkn },
+  } = router;
 
-    // useEffect(() => {
-    //   const queryString = window.location.search;
-    //   const urlParams = new URLSearchParams(queryString);
-    //   const gatherWindow = urlParams.get("gather_window");
-    //   const roomType = urlParams.get("room_type");
-    //   if (gatherWindow) setGatherWindow(gatherWindow);
-    //   if (roomType) {
-    //     changePageHandler(roomType);
-    //   }
-    // }, []);
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const gatherWindow = urlParams.get("gather_window");
+    const roomType = urlParams.get("room_type");
 
-    // useEffect(() => {
-    //   const showFirstTimeUser = Cookies.get("showFirstTimeUser");
-
-    //   if (inviteTKN || tkn) {
-    //     if (showFirstTimeUser) {
-    //       Cookies.remove("showFirstTimeUser");
-    //     }
-    //     setShowCreateHarthNameModal(false);
-    //     setShowInviteAcceptModal(true);
-    //   }
-    //   return () => {
-    //     setShowInviteAcceptModal(false);
-    //   };
-    // }, [inviteTKN, tkn]);
-
-    useEffect(() => {
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const gatherWindow = urlParams.get("gather_window");
-        const roomType = urlParams.get("room_type");
-
-        if (!loading) {
-            if (!user) {
-                router.push("/");
-            } else if (roomType) {
-                changePageHandler(roomType);
-            } else {
-                changePageHandler("chat");
-                const showFirstTimeUser = Cookies.get("showFirstTimeUser");
-                if (showFirstTimeUser) {
-                    sendWelcomeEmailToUser({
-                        user,
-                        subject: "Welcome To Harth",
-                    });
-                    if (!inviteTKN && !tkn) {
-                        setShowCreateHarthNameModal(true);
-                    } else {
-                        Cookies.remove("showFirstTimeUser");
-                        setShowCreateHarthNameModal(false);
-                        setShowInviteAcceptModal(true);
-                    }
-                } else {
-                    if (inviteTKN || tkn) {
-                        setShowCreateHarthNameModal(false);
-                        setShowInviteAcceptModal(true);
-                    }
-                }
-            }
-            if (gatherWindow) setGatherWindow(gatherWindow);
-        }
-    }, [loading]);
-
-    // useEffect(() => {
-    //   if (!loading && !user) router.push("/");
-    // }, [loading]);
-
-    const changePageHandler = (pg) => {
-        setCurrentPage(pg);
-    };
-    const harthNameCreationHandler = async (harth) => {
-        setNewHarth(harth);
-        setShowCreateHarthNameModal(false);
-        setShowCreateHarthProfileModal(true);
-    };
-    const resetNewHarth = () => {
+    if (!loading) {
+      if (!user) {
+        router.push("/");
+      } else if (roomType) {
+        changePageHandler(roomType);
+      } else {
+        changePageHandler("chat");
         const showFirstTimeUser = Cookies.get("showFirstTimeUser");
         if (showFirstTimeUser) {
+          sendWelcomeEmailToUser({
+            user,
+            subject: "Welcome To Harth",
+          });
+          if (!inviteTKN && !tkn) {
+            setShowCreateHarthNameModal(true);
+          } else {
             Cookies.remove("showFirstTimeUser");
-        }
-        setNewHarth(null);
-        setShowCreateHarthProfileModal(false);
-        if (inviteTKN || tkn) {
             setShowCreateHarthNameModal(false);
             setShowInviteAcceptModal(true);
+          }
+        } else {
+          if (inviteTKN || tkn) {
+            async function testToken() {
+              let results = await checkIfInviteTokenIsGood({
+                token: tkn,
+                user,
+              });
+              console.log(results);
+              if (results.ok) {
+                setShowCreateHarthNameModal(false);
+                setShowInviteAcceptModal(true);
+              }
+            }
+            testToken();
+          }
         }
-    };
-    const resetNewInviteHarth = () => {
-        setInvitedHarth(null);
-        setShowInviteProfileModal(false);
-    };
-    const goodInviteHandler = (harth) => {
-        console.log(harth, "goodInviteHandler");
-        setShowInviteAcceptModal(false);
-        setInvitedHarth({ ...harth });
-        setShowInviteProfileModal(true);
-    };
-
-    if (user && currentPage) {
-        let page;
-        switch (currentPage) {
-            case "chat":
-                page = <Chat />;
-                break;
-            case "gather":
-                page = (
-                    <VideoProvider>
-                        <Video />
-                    </VideoProvider>
-                );
-                break;
-            case "party":
-                page = <Party />;
-                break;
-            case "voice":
-                page = <Voice />;
-                break;
-            case "stream":
-                page = <Stream />;
-                break;
-            case "message":
-                page = <Message />;
-                break;
-            default:
-                page = <Chat />;
-                break;
-        }
-        return (
-            <CommsProvider>
-                <ChatProvider>
-                    <SocketProvider>
-                        {showCreateHarthNameModal ? (
-                            <CreateHarthName
-                                talkingHeadMsg="Give your härth a name and and image"
-                                footer="Tip: You can change your härth name and image at any time"
-                                placeholder="härth name"
-                                submitText="Create"
-                                closeHandler={() =>
-                                    setShowCreateHarthNameModal(false)
-                                }
-                                submitHandler={harthNameCreationHandler}
-                            />
-                        ) : null}
-                        {showCreateHarthProfileModal ? (
-                            <CreateHarthProfile
-                                talkingHeadMsg="Enter the name you would like to be called in your new härth and add a profile picture"
-                                footer="Familiar Tip: You can change your profile name nad picture at any time"
-                                placeholder="your profile name"
-                                submitText="Join"
-                                submitHandler={resetNewHarth}
-                                harth={newHarth}
-                            />
-                        ) : null}
-                        {showInviteAcceptModal ? (
-                            <HarthInviteAcceptModal
-                                talkingHeadMsg="An invitation arrives. Do you accept?"
-                                footer="You have been invited to join this harth by [profile name]"
-                                submitText="Accept Invite"
-                                submitHandler={goodInviteHandler}
-                                tkn={tkn || inviteTKN || ""}
-                            />
-                        ) : null}
-                        {showInviteProfileModal ? (
-                            <CreateHarthProfile
-                                header="harth"
-                                talkingHeadMsg="And by what name would you like to be known"
-                                footer="Set your name and profile pic for this harth. You can change these at any time"
-                                placeholder={`${"First Name"}`}
-                                submitText="Join"
-                                submitHandler={resetNewInviteHarth}
-                                harth={invitedHarth}
-                                invite={true}
-                            />
-                        ) : null}
-                        {GatherWindow ? (
-                            page
-                        ) : (
-                            <DashboardLayout
-                                changePage={changePageHandler}
-                                currentPage={currentPage}
-                                setShowCreateHarthNameModal={
-                                    setShowCreateHarthNameModal
-                                }
-                            >
-                                {page}
-                            </DashboardLayout>
-                        )}
-                    </SocketProvider>
-                </ChatProvider>
-            </CommsProvider>
-        );
+      }
+      if (gatherWindow) setGatherWindow(gatherWindow);
     }
+  }, [loading]);
 
-    return <p>...loading</p>;
+  const changePageHandler = (pg) => {
+    setCurrentPage(pg);
+  };
+  const harthNameCreationHandler = async (harth) => {
+    setNewHarth(harth);
+    setShowCreateHarthNameModal(false);
+    setShowCreateHarthProfileModal(true);
+  };
+  const resetNewHarth = () => {
+    const showFirstTimeUser = Cookies.get("showFirstTimeUser");
+    if (showFirstTimeUser) {
+      Cookies.remove("showFirstTimeUser");
+    }
+    setNewHarth(null);
+    setShowCreateHarthProfileModal(false);
+    if (inviteTKN || tkn) {
+      setShowCreateHarthNameModal(false);
+      setShowInviteAcceptModal(true);
+    }
+  };
+  const resetNewInviteHarth = () => {
+    setInvitedHarth(null);
+    setShowInviteProfileModal(false);
+  };
+  const goodInviteHandler = (harth) => {
+    console.log(harth, "goodInviteHandler");
+    setShowInviteAcceptModal(false);
+    setInvitedHarth({ ...harth });
+    setShowInviteProfileModal(true);
+  };
+
+  if (user && currentPage) {
+    let page;
+    switch (currentPage) {
+      case "chat":
+        page = <Chat />;
+        break;
+      case "gather":
+        page = (
+          <VideoProvider>
+            <Video />
+          </VideoProvider>
+        );
+        break;
+      case "party":
+        page = <Party />;
+        break;
+      case "voice":
+        page = <Voice />;
+        break;
+      case "stream":
+        page = <Stream />;
+        break;
+      case "message":
+        page = <Message />;
+        break;
+      default:
+        page = <Chat />;
+        break;
+    }
+    return (
+      <CommsProvider>
+        <ChatProvider>
+          <SocketProvider>
+            {showCreateHarthNameModal ? (
+              <CreateHarthName
+                talkingHeadMsg="Give your härth a name and and image"
+                footer="Tip: You can change your härth name and image at any time"
+                placeholder="härth name"
+                submitText="Create"
+                closeHandler={() => setShowCreateHarthNameModal(false)}
+                submitHandler={harthNameCreationHandler}
+              />
+            ) : null}
+            {showCreateHarthProfileModal ? (
+              <CreateHarthProfile
+                talkingHeadMsg="Enter the name you would like to be called in your new härth and add a profile picture"
+                footer="Familiar Tip: You can change your profile name nad picture at any time"
+                placeholder="your profile name"
+                submitText="Join"
+                submitHandler={resetNewHarth}
+                harth={newHarth}
+              />
+            ) : null}
+            {showInviteAcceptModal ? (
+              <HarthInviteAcceptModal
+                talkingHeadMsg="An invitation arrives. Do you accept?"
+                footer="You have been invited to join this harth by [profile name]"
+                submitText="Accept Invite"
+                submitHandler={goodInviteHandler}
+                tkn={tkn || inviteTKN || ""}
+                user={user}
+              />
+            ) : null}
+            {showInviteProfileModal ? (
+              <CreateHarthProfile
+                header="harth"
+                talkingHeadMsg="And by what name would you like to be known"
+                footer="Set your name and profile pic for this harth. You can change these at any time"
+                placeholder={`${"First Name"}`}
+                submitText="Join"
+                submitHandler={resetNewInviteHarth}
+                harth={invitedHarth}
+                invite={true}
+              />
+            ) : null}
+            {GatherWindow ? (
+              page
+            ) : (
+              <DashboardLayout
+                changePage={changePageHandler}
+                currentPage={currentPage}
+                setShowCreateHarthNameModal={setShowCreateHarthNameModal}
+                user={user}
+              >
+                {page}
+              </DashboardLayout>
+            )}
+          </SocketProvider>
+        </ChatProvider>
+      </CommsProvider>
+    );
+  }
+
+  return <p>...loading</p>;
 };
 
 export default dashboard;
