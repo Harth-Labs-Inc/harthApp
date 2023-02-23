916e788030122673a4d004899ea795c0f7e51947
philjackson91 <48493085+philjackson91@users.noreply.github.com>
2023-02-23 13:06:29 -0500

    bad route fix

--- pages/comm/index.js
+++ pages/comm/index.js
@@ -15,208 +15,208 @@
 // import SubmitCom from "./submitCom";
 // import JoinCom from "./joinCom";
 
-// const CommIndexPage = () => {
-//   return <p>bad route</p>;
-//   const [currentPage, setCurrentPage] = useState("initial");
-//   const [comID, setcomID] = useState();
-//   const [community, setCommunity] = useState({ comIcon: {}, comName: "" });
-//   const [profile, setProfile] = useState({ profIcon: {}, profName: "" });
-//   const [personalInfo, setPersonalInfo] = useState({});
+const CommIndexPage = () => {
+  return <p>bad route</p>;
+  const [currentPage, setCurrentPage] = useState("initial");
+  const [comID, setcomID] = useState();
+  const [community, setCommunity] = useState({ comIcon: {}, comName: "" });
+  const [profile, setProfile] = useState({ profIcon: {}, profName: "" });
+  const [personalInfo, setPersonalInfo] = useState({});
 
-//   const router = useRouter();
-//   const { user } = useAuth();
+  const router = useRouter();
+  const { user } = useAuth();
 
-//   const {
-//     pathname,
-//     query: { tkn },
-//   } = router;
+  const {
+    pathname,
+    query: { tkn },
+  } = router;
 
-//   useEffect(async () => {
-//     if (tkn) {
-//       setCurrentPage("profile");
-//       const data = await getCommFromInvite(tkn);
-//       const { ok, comm } = data;
-//       console.log(data);
-//       if (ok) {
-//         setCommFromInvite(comm.comm_id);
-//       }
-//     }
-//   }, [tkn]);
+  useEffect(async () => {
+    if (tkn) {
+      setCurrentPage("profile");
+      const data = await getCommFromInvite(tkn);
+      const { ok, comm } = data;
+      console.log(data);
+      if (ok) {
+        setCommFromInvite(comm.comm_id);
+      }
+    }
+  }, [tkn]);
 
-//   useEffect(() => {
-//     Router.prefetch("/");
-//   }, []);
+  useEffect(() => {
+    Router.prefetch("/");
+  }, []);
 
-//   const setCommFromInvite = (id) => {
-//     setcomID(id);
-//   };
+  const setCommFromInvite = (id) => {
+    setcomID(id);
+  };
 
-//   const commHandler = (data) => {
-//     setCommunity(data);
-//   };
+  const commHandler = (data) => {
+    setCommunity(data);
+  };
 
-//   const profHandler = (data) => {
-//     setProfile(data);
-//   };
-//   const persHandler = (data) => {
-//     setPersonalInfo(data);
-//   };
+  const profHandler = (data) => {
+    setProfile(data);
+  };
+  const persHandler = (data) => {
+    setPersonalInfo(data);
+  };
 
-//   const uploadFile = (file, bucket) => {
-//     return new Promise((resolve) => {
-//       if (file.name) {
-//         (async () => {
-//           let extention = file.name.split(".").pop();
-//           let id = generateID();
-//           let name = `${id}.${extention}`;
+  const uploadFile = (file, bucket) => {
+    return new Promise((resolve) => {
+      if (file.name) {
+        (async () => {
+          let extention = file.name.split(".").pop();
+          let id = generateID();
+          let name = `${id}.${extention}`;
 
-//           const data = await getUploadURL(name, file.type, bucket);
-//           const { ok, msg, uploadURL } = data;
-//           if (ok) {
-//             let reader = new FileReader();
-//             reader.addEventListener("loadend", async (event) => {
-//               let result = await putImageInBucket(uploadURL, reader, file.type);
-//               let { status } = result;
-//               if (status == 200) {
-//                 resolve({ ok: 1, name });
-//               } else {
-//                 resolve({ ok: 0 });
-//               }
-//             });
-//             reader.readAsArrayBuffer(file);
-//           }
-//         })();
-//       } else {
-//         resolve({ ok: 1 });
-//       }
-//     });
-//   };
+          const data = await getUploadURL(name, file.type, bucket);
+          const { ok, msg, uploadURL } = data;
+          if (ok) {
+            let reader = new FileReader();
+            reader.addEventListener("loadend", async (event) => {
+              let result = await putImageInBucket(uploadURL, reader, file.type);
+              let { status } = result;
+              if (status == 200) {
+                resolve({ ok: 1, name });
+              } else {
+                resolve({ ok: 0 });
+              }
+            });
+            reader.readAsArrayBuffer(file);
+          }
+        })();
+      } else {
+        resolve({ ok: 1 });
+      }
+    });
+  };
 
-//   const changePageHandler = (pg) => {
-//     setCurrentPage(pg);
-//   };
+  const changePageHandler = (pg) => {
+    setCurrentPage(pg);
+  };
 
-//   const createComm = async () => {
-//     if (!comID) {
-//       let comms3Upload;
-//       let commDbUpload;
-//       let profs3Upload;
-//       let profDbUpload;
+  const createComm = async () => {
+    if (!comID) {
+      let comms3Upload;
+      let commDbUpload;
+      let profs3Upload;
+      let profDbUpload;
 
-//       if (community.image && community.image.name) {
-//         comms3Upload = await uploadFile(
-//           community.image,
-//           "community-profile-images"
-//         );
-//       }
-//       if (profile.image && profile.image.name) {
-//         profs3Upload = await uploadFile(profile.image, "community-user-images");
-//         if (profs3Upload.ok) {
-//         }
-//       }
-//       commDbUpload = await saveCommunity({
-//         name: community.comName,
-//         iconKey:
-//           comms3Upload && comms3Upload.name
-//             ? `https://community-profile-images.s3.us-east-2.amazonaws.com/${comms3Upload.name}`
-//             : "",
-//         users: [],
-//         topics: [],
-//       });
-//       console.log("commDbUpload", commDbUpload);
-//       if (commDbUpload.ok) {
-//         profDbUpload = await addUserToComm(commDbUpload.id, {
-//           userId: user._id,
-//           iconKey:
-//             profs3Upload && profs3Upload.name
-//               ? `https://community-user-images.s3.us-east-2.amazonaws.com/${profs3Upload.name}`
-//               : "",
-//           name: profile.profName,
-//           personalInfo: personalInfo,
-//         });
-//         console.log(profDbUpload);
-//         if (profDbUpload.ok) {
-//           window.history.replaceState(null, "", "/");
-//           window.location.pathname = "/";
-//         }
-//       }
-//     } else {
-//       let profs3Upload;
-//       let profDbUpload;
+      if (community.image && community.image.name) {
+        comms3Upload = await uploadFile(
+          community.image,
+          "community-profile-images"
+        );
+      }
+      if (profile.image && profile.image.name) {
+        profs3Upload = await uploadFile(profile.image, "community-user-images");
+        if (profs3Upload.ok) {
+        }
+      }
+      commDbUpload = await saveCommunity({
+        name: community.comName,
+        iconKey:
+          comms3Upload && comms3Upload.name
+            ? `https://community-profile-images.s3.us-east-2.amazonaws.com/${comms3Upload.name}`
+            : "",
+        users: [],
+        topics: [],
+      });
+      console.log("commDbUpload", commDbUpload);
+      if (commDbUpload.ok) {
+        profDbUpload = await addUserToComm(commDbUpload.id, {
+          userId: user._id,
+          iconKey:
+            profs3Upload && profs3Upload.name
+              ? `https://community-user-images.s3.us-east-2.amazonaws.com/${profs3Upload.name}`
+              : "",
+          name: profile.profName,
+          personalInfo: personalInfo,
+        });
+        console.log(profDbUpload);
+        if (profDbUpload.ok) {
+          window.history.replaceState(null, "", "/");
+          window.location.pathname = "/";
+        }
+      }
+    } else {
+      let profs3Upload;
+      let profDbUpload;
 
-//       if (profile.image && profile.image.name) {
-//         profs3Upload = await uploadFile(profile.image, "community-user-images");
-//       }
+      if (profile.image && profile.image.name) {
+        profs3Upload = await uploadFile(profile.image, "community-user-images");
+      }
 
-//       profDbUpload = await addUserToComm(comID, {
-//         userId: user._id,
-//         iconKey:
-//           profs3Upload && profs3Upload.name
-//             ? `https://community-user-images.s3.us-east-2.amazonaws.com/${profs3Upload.name}`
-//             : "",
-//         name: profile.profName,
-//         personalInfo: personalInfo,
-//       });
-//       console.log(profDbUpload);
-//       if (profDbUpload.ok) {
-//         window.history.replaceState(null, "", "/");
-//         window.location.pathname = "/";
-//       }
-//     }
-//   };
+      profDbUpload = await addUserToComm(comID, {
+        userId: user._id,
+        iconKey:
+          profs3Upload && profs3Upload.name
+            ? `https://community-user-images.s3.us-east-2.amazonaws.com/${profs3Upload.name}`
+            : "",
+        name: profile.profName,
+        personalInfo: personalInfo,
+      });
+      console.log(profDbUpload);
+      if (profDbUpload.ok) {
+        window.history.replaceState(null, "", "/");
+        window.location.pathname = "/";
+      }
+    }
+  };
 
-//   let page;
-//   switch (currentPage) {
-//     case "create":
-//       page = (
-//         <CreateCom
-//           changePage={changePageHandler}
-//           onCommChange={commHandler}
-//         ></CreateCom>
-//       );
-//       break;
-//     case "invite":
-//       page = (
-//         <JoinCom
-//           changePage={changePageHandler}
-//           onCommChange={setCommFromInvite}
-//         ></JoinCom>
-//       );
-//       break;
-//     case "profile":
-//       page = (
-//         <CreateProf
-//           changePage={changePageHandler}
-//           commData={community}
-//           user={user}
-//           onProfChange={profHandler}
-//           onCommChange={commHandler}
-//           onPersChange={persHandler}
-//         ></CreateProf>
-//       );
-//       break;
-//     case "submit":
-//       page = (
-//         <SubmitCom
-//           changePage={changePageHandler}
-//           commData={community}
-//           user={user}
-//           onProfChange={profHandler}
-//           onCreate={createComm}
-//         ></SubmitCom>
-//       );
-//       break;
-//     default:
-//       page = (
-//         <InitialCom
-//           pathname={pathname}
-//           changePage={changePageHandler}
-//         ></InitialCom>
-//       );
-//       break;
-//   }
+  let page;
+  switch (currentPage) {
+    case "create":
+      page = (
+        <CreateCom
+          changePage={changePageHandler}
+          onCommChange={commHandler}
+        ></CreateCom>
+      );
+      break;
+    case "invite":
+      page = (
+        <JoinCom
+          changePage={changePageHandler}
+          onCommChange={setCommFromInvite}
+        ></JoinCom>
+      );
+      break;
+    case "profile":
+      page = (
+        <CreateProf
+          changePage={changePageHandler}
+          commData={community}
+          user={user}
+          onProfChange={profHandler}
+          onCommChange={commHandler}
+          onPersChange={persHandler}
+        ></CreateProf>
+      );
+      break;
+    case "submit":
+      page = (
+        <SubmitCom
+          changePage={changePageHandler}
+          commData={community}
+          user={user}
+          onProfChange={profHandler}
+          onCreate={createComm}
+        ></SubmitCom>
+      );
+      break;
+    default:
+      page = (
+        <InitialCom
+          pathname={pathname}
+          changePage={changePageHandler}
+        ></InitialCom>
+      );
+      break;
+  }
 
-//   return page;
-// };
+  return page;
+};
 
-// export default CommIndexPage;
+export default CommIndexPage;
