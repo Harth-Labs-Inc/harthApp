5a6c21c8df38c514c5c11f74cbff78ed639a67c8
Jorge Piquer <piquerjr@gmail.com>
2023-03-10 13:15:11 -0500

    add voice room, image loader

--- components/ChatInput/ImageHolder.js
+++ components/ChatInput/ImageHolder.js
@@ -3,33 +3,38 @@ import { IconClose } from "../../resources/icons/IconClose";
 import styles from "./ChatInput.module.scss";
 
 const ImageHolder = ({ attachments, removeAttachment, attRefs, uploading }) => {
-  if (attachments.length > 0) {
-    return (
-      <div className={styles.imageBar}>
-        {(attachments || []).map((file, idx) => (
-          <div className={styles.imageContainer} key={file?.name}>
-            <button
-              onClick={() => {
-                removeAttachment(idx);
-              }}
-            >
-              <IconClose />
-            </button>
-            <img
-              id={file.name}
-              key={file.name}
-              ref={(el) => (attRefs.current[idx] = el)}
-              alt=""
-              loading="lazy"
-            />
-            {uploading.includes(file.name) ? <p>uploading...</p> : null}
-          </div>
-        ))}
-      </div>
-    );
-  }
+    if (attachments.length > 0) {
+        return (
+            <div className={styles.imageBar}>
+                {(attachments || []).map((file, idx) => (
+                    <div
+                        className={`${styles.imageContainer} ${
+                            uploading.includes(file.name) &&
+                            styles.imageUploading
+                        }`}
+                        key={file?.name}
+                    >
+                        <button
+                            onClick={() => {
+                                removeAttachment(idx);
+                            }}
+                        >
+                            <IconClose />
+                        </button>
+                        <img
+                            id={file.name}
+                            key={file.name}
+                            ref={(el) => (attRefs.current[idx] = el)}
+                            alt=""
+                            loading="lazy"
+                        />
+                    </div>
+                ))}
+            </div>
+        );
+    }
 
-  return null;
+    return null;
 };
 
 export default ImageHolder;
