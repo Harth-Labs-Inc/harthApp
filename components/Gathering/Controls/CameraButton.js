d52366e95da9d85d3945d93ae09232354869f38b
philjackson91 <philjackson91.pj@gmail.com>
2023-03-22 13:17:33 -0400

    room-updates

--- components/Gathering/Controls/CameraButton.js
+++ components/Gathering/Controls/CameraButton.js
@@ -1,58 +1,64 @@
 import { IconVideoFill } from "../../../resources/icons/IconVideoFill";
 import { IconVideoOffFill } from "../../../resources/icons/IconVideoOffFill";
 import styles from "./gatheringButtons.module.scss";
+import OutsideClickHandler from "components/Common/Modals/OutsideClick";
 
 export const CameraButton = (props) => {
-    const {
-        isMobile = false,
-        onPress,
-        videoList,
-        changeVideoDevice,
-        clearVideoList,
-        isOn,
-    } = props;
+  const {
+    isMobile = false,
+    onPress,
+    videoList,
+    changeVideoDevice,
+    clearVideoList,
+    isOn,
+  } = props;
 
-    return (
-        <div>
-            {videoList ? (
-                <div className={styles.OptionsMenu}>
-                    <div className={styles.OptionsMenuContainer}>
-                        {videoList.map((device) => {
-                            const { label, deviceId } = device;
-                            return (
-                                <button
-                                    key={deviceId}
-                                    onClick={() => {
-                                        clearVideoList();
-                                        changeVideoDevice(device);
-                                    }}
-                                >
-                                    {label}
-                                </button>
-                            );
-                        })}
-                    </div>
-                </div>
-            ) : null}
-            <button
-                className={`
+  return (
+    <div>
+      {videoList ? (
+        <OutsideClickHandler
+          onClickOutside={clearVideoList}
+          onFocusOutside={clearVideoList}
+        >
+          <div className={styles.OptionsMenu}>
+            <div className={styles.OptionsMenuContainer}>
+              {videoList.map((device) => {
+                const { label, deviceId } = device;
+                return (
+                  <button
+                    key={deviceId}
+                    onClick={() => {
+                      clearVideoList();
+                      changeVideoDevice(device);
+                    }}
+                  >
+                    {label}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </OutsideClickHandler>
+      ) : null}
+      <button
+        className={`
                 ${styles.basicButton} 
                 ${isMobile ? styles.basicButtonLarge : styles.basicButtonSmall} 
                 ${isOn ? styles.basicButtonActive : styles.basicButtonInactive}
             `}
-                aria-label="Webcam"
-                onClick={onPress}
-            >
-                {isOn ? (
-                    <div height="100%" width="100%">
-                        <IconVideoFill hasGradient="true" />
-                    </div>
-                ) : (
-                    <div height="100%" width="100%">
-                        <IconVideoOffFill fill="#bb0000" />
-                    </div>
-                )}
-            </button>
-        </div>
-    );
+        aria-label="Webcam"
+        onClick={onPress}
+      >
+        {isOn ? (
+          <div height="100%" width="100%">
+            <IconVideoFill hasGradient="true" />
+          </div>
+        ) : (
+          <div height="100%" width="100%">
+            <IconVideoOffFill fill="#bb0000" />
+          </div>
+        )}
+      </button>
+    </div>
+  );
 };
--- components/Gathering/GatherControlBar/GatherControlBar.js
+++ components/Gathering/GatherControlBar/GatherControlBar.js
@@ -1,13 +1,13 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { MobileContext } from "../../../contexts/mobile";
 import {
-    BagButton,
-    CameraButton,
-    ChatButton,
-    LeaveButton,
-    MicButton,
-    MoreButton,
-    StreamButton,
+  BagButton,
+  CameraButton,
+  ChatButton,
+  LeaveButton,
+  MicButton,
+  MoreButton,
+  StreamButton,
 } from "../Controls";
 import { DiceBar } from "../GatherTools/DiceBar";
 import { MapBar } from "../GatherTools/MapBar";
@@ -16,203 +16,227 @@ import { Modal } from "../../Common";
 import styles from "./gatheringControlBar.module.scss";
 
 const GatherControlBar = (props) => {
-    const {
-        roomType = "party",
-        onLeaveHandler,
-        onToggleVideo,
-        onToggleAudio,
-        onToggleScreenShare,
-        captureIsActice,
-        onToggleChat,
-        diceRollHandler,
-        changeAudioDevice,
-        changeVideoDevice,
-        unreadMsg,
-        roomId,
-        userInfo,
-        mapChangeHandler,
-        mapUpdate,
-    } = props;
-    const [modal, setModal] = useState();
-    const [showDiceModal, setShowDiceModal] = useState();
-    const [showMapModal, setShowMapModal] = useState();
-    const [audioList, setAudioList] = useState();
-    const [videoList, setVideoList] = useState();
+  const {
+    roomType = "party",
+    onLeaveHandler,
+    onToggleVideo,
+    onToggleAudio,
+    onToggleScreenShare,
+    captureIsActice,
+    onToggleChat,
+    diceRollHandler,
+    changeAudioDevice,
+    changeVideoDevice,
+    unreadMsg,
+    roomId,
+    userInfo,
+    mapChangeHandler,
+    mapUpdate,
+    hasAudioStream,
+    hasVideoStream,
+    hasScreenShareStream,
+  } = props;
+  const [modal, setModal] = useState();
+  const [showDiceModal, setShowDiceModal] = useState();
+  const [showMapModal, setShowMapModal] = useState();
+  const [audioList, setAudioList] = useState();
+  const [videoList, setVideoList] = useState();
 
-    const { isMobile } = useContext(MobileContext);
+  const AudioDeviceListCount = useRef(0);
+  const VideoDeviceListCount = useRef(0);
 
-    const showModal = () => {
-        setModal(!modal);
-    };
+  const { isMobile } = useContext(MobileContext);
 
-    const togggleDiceModal = () => {
-        setShowDiceModal((prevState) => !prevState);
-    };
-    const togggleMapModal = () => {
-        setShowMapModal((prevState) => !prevState);
-    };
+  const showModal = () => {
+    setModal(!modal);
+  };
 
-    const onToggleAudioDevicesModal = () => {
-        if (!audioList) {
-            setAudioList(null);
-        } /*else if (!navigator.mediaDevices?.enumerateDevices) {
-        }*/ else {
-            navigator.mediaDevices
-                .enumerateDevices()
-                .then((devices) => {
-                    let audioInputs = devices.filter(
-                        ({ kind }) => kind == "audioinput"
-                    );
-                    setAudioList(audioInputs);
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-        }
-    };
+  const togggleDiceModal = () => {
+    setShowDiceModal((prevState) => !prevState);
+  };
+  const togggleMapModal = () => {
+    setShowMapModal((prevState) => !prevState);
+  };
 
-    const onToggleVideoDevicesModal = () => {
-        if (!videoList) {
-            setVideoList(null);
-        } /*else if (!navigator.mediaDevices?.enumerateDevices) {
-        }*/ else {
-            navigator.mediaDevices
-                .enumerateDevices()
-                .then((devices) => {
-                    let videoInputs = devices.filter(
-                        ({ kind }) => kind == "videoinput"
-                    );
-                    setVideoList(videoInputs);
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-        }
-    };
+  const onToggleAudioDevicesModal = () => {
+    if (audioList) {
+      setAudioList(null);
+    } else {
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+          let audioInputs = devices.filter(({ kind }) => kind == "audioinput");
+          setAudioList(audioInputs);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
 
-    return (
-        <>
-            <div className="conditionals">
-                {showMapModal ? (
-                    <MapBar
-                        togggleMapModal={togggleMapModal}
-                        roomId={roomId}
-                        mapChangeHandler={mapChangeHandler}
-                        mapUpdate={mapUpdate}
-                    />
-                ) : null}
-                {showDiceModal ? (
-                    <DiceBar
-                        diceRollHandler={diceRollHandler}
-                        togggleDiceModal={togggleDiceModal}
-                    />
-                ) : null}
-                {modal ? (
-                    <Modal show={modal} onToggleModal={showModal}>
-                        <div>something</div>
-                    </Modal>
-                ) : (
-                    ""
-                )}
-            </div>
+  const onToggleVideoDevicesModal = () => {
+    if (videoList) {
+      setVideoList(null);
+    } else {
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then((devices) => {
+          let videoInputs = devices.filter(({ kind }) => kind == "videoinput");
+          setVideoList(videoInputs);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
 
-            {isMobile ? (
-                <header className={styles.mobile}>
-                    {roomType == "voice" && (
-                        <div className={styles.spacerLarge} />
-                    )}
-                    {roomType == "party" && <BagButton isMobile={true} />}
-                    {roomType != "voice" && (
-                        <CameraButton
-                            onPress={onToggleVideo}
-                            videoList={videoList}
-                            changeVideoDevice={changeVideoDevice}
-                            clearVideoList={() => setVideoList(null)}
-                            isMobile={true}
-                        />
-                    )}
-                    <MicButton
-                        onPress={onToggleAudio}
-                        audioList={audioList}
-                        changeAudioDevice={changeAudioDevice}
-                        clearAudioList={() => setAudioList(null)}
-                        isMobile={true}
-                    />
-                    {roomType != "voice" && (
-                        <>
-                            {/* <StreamButton
+  if (!navigator.mediaDevices?.enumerateDevices) {
+  } else {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) => {
+        let tempAudioList = [];
+        let tempVideoList = [];
+        devices.forEach((device) => {
+          if (device.kind.includes("audio")) {
+            tempAudioList.push(device);
+          }
+          if (device.kind.includes("video")) {
+            tempVideoList.push(device);
+          }
+        });
+        AudioDeviceListCount.current = tempAudioList.length || 0;
+        VideoDeviceListCount.current = tempVideoList.length || 0;
+      })
+      .catch((err) => {
+        AudioDeviceListCount.current = 0;
+        VideoDeviceListCount.current = 0;
+      });
+  }
+
+  return (
+    <>
+      <div className="conditionals">
+        {showMapModal ? (
+          <MapBar
+            togggleMapModal={togggleMapModal}
+            roomId={roomId}
+            mapChangeHandler={mapChangeHandler}
+            mapUpdate={mapUpdate}
+          />
+        ) : null}
+        {showDiceModal ? (
+          <DiceBar
+            diceRollHandler={diceRollHandler}
+            togggleDiceModal={togggleDiceModal}
+          />
+        ) : null}
+        {modal ? (
+          <Modal show={modal} onToggleModal={showModal}>
+            <div>something</div>
+          </Modal>
+        ) : (
+          ""
+        )}
+      </div>
+
+      {isMobile ? (
+        <header className={styles.mobile}>
+          {roomType == "voice" && <div className={styles.spacerLarge} />}
+          {roomType == "party" && <BagButton isMobile={true} />}
+          {roomType != "voice" && (
+            <CameraButton
+              onPress={onToggleVideo}
+              videoList={videoList}
+              changeVideoDevice={changeVideoDevice}
+              clearVideoList={() => setVideoList(null)}
+              isMobile={true}
+            />
+          )}
+          <MicButton
+            onPress={onToggleAudio}
+            audioList={audioList}
+            changeAudioDevice={changeAudioDevice}
+            clearAudioList={() => setAudioList(null)}
+            isMobile={true}
+          />
+          {roomType != "voice" && (
+            <>
+              {/* <StreamButton
                                 onPress={onToggleScreenShare}
                                 show={captureIsActice}
                                 isMobile={true}
                             /> */}
-                            <ChatButton
-                                unreadMsg={unreadMsg}
-                                onPress={onToggleChat}
-                                isMobile={true}
-                            />
-                        </>
-                    )}
-                </header>
-            ) : (
-                <header className={styles.desktop}>
-                    <LeaveButton onPress={onLeaveHandler} />
+              <ChatButton
+                unreadMsg={unreadMsg}
+                onPress={onToggleChat}
+                isMobile={true}
+              />
+            </>
+          )}
+        </header>
+      ) : (
+        <header className={styles.desktop}>
+          <LeaveButton onPress={onLeaveHandler} />
 
-                    <div className={styles.middleGroup}>
-                        {roomType == "party" ? (
-                            <div className={styles.optionsContainer}>
-                                <div className={styles.mainButton}>
-                                    <CameraButton
-                                        onPress={onToggleVideo}
-                                        videoList={videoList}
-                                        changeVideoDevice={changeVideoDevice}
-                                        clearVideoList={() =>
-                                            setVideoList(null)
-                                        }
-                                        isOn={userInfo?.video}
-                                    />
-                                </div>
-                                <div className={styles.moreButton}>
-                                    <MoreButton
-                                        onPress={onToggleVideoDevicesModal}
-                                        isActive={!!videoList}
-                                    />
-                                </div>
-                            </div>
-                        ) : null}
+          <div className={styles.middleGroup}>
+            {roomType == "party" ? (
+              <div className={styles.optionsContainer}>
+                <div className={styles.mainButton}>
+                  <CameraButton
+                    onPress={onToggleVideo}
+                    videoList={videoList}
+                    changeVideoDevice={changeVideoDevice}
+                    clearVideoList={(e) => setVideoList(null)}
+                    isOn={hasVideoStream}
+                  />
+                </div>
+                {VideoDeviceListCount.current > 1 ? (
+                  <div className={styles.moreButton}>
+                    <MoreButton
+                      onPress={onToggleVideoDevicesModal}
+                      isActive={!!videoList}
+                    />
+                  </div>
+                ) : null}
+              </div>
+            ) : null}
 
-                        <div className={styles.optionsContainer}>
-                            <div className={styles.mainButton}>
-                                <MicButton
-                                    onPress={onToggleAudio}
-                                    audioList={audioList}
-                                    changeAudioDevice={changeAudioDevice}
-                                    clearAudioList={() => setAudioList(null)}
-                                    isOn={userInfo?.audio}
-                                />
-                            </div>
-                            <div className={styles.moreButton}>
-                                <MoreButton
-                                    onPress={onToggleAudioDevicesModal}
-                                    isActive={!!audioList}
-                                />
-                            </div>
-                        </div>
-                        {roomType !== "voice" && (
-                            <StreamButton
-                                onPress={onToggleScreenShare}
-                                show={captureIsActice}
-                                isOn={userInfo?.screenShare}
-                            />
-                        )}
-                        {roomType == "party" && (
-                            <BagButton
-                                onDicePress={togggleDiceModal}
-                                onMapPress={togggleMapModal}
-                            />
-                        )}
-                    </div>
+            <div className={styles.optionsContainer}>
+              <div className={styles.mainButton}>
+                <MicButton
+                  onPress={onToggleAudio}
+                  audioList={audioList}
+                  changeAudioDevice={changeAudioDevice}
+                  clearAudioList={() => setAudioList(null)}
+                  isOn={hasAudioStream}
+                />
+              </div>
+              {AudioDeviceListCount.current > 1 ? (
+                <div className={styles.moreButton}>
+                  <MoreButton
+                    onPress={onToggleAudioDevicesModal}
+                    isActive={!!audioList}
+                  />
+                </div>
+              ) : null}
+            </div>
+            {roomType !== "voice" && (
+              <StreamButton
+                onPress={onToggleScreenShare}
+                show={captureIsActice}
+                isOn={hasScreenShareStream}
+              />
+            )}
+            {roomType == "party" && (
+              <BagButton
+                onDicePress={togggleDiceModal}
+                onMapPress={togggleMapModal}
+              />
+            )}
+          </div>
 
-                    {/* {roomType != "voice" ? (
+          {/* {roomType != "voice" ? (
                         
                     ) : (
                         <div className={styles.optionsContainer}>
@@ -225,18 +249,15 @@ const GatherControlBar = (props) => {
                         </div>
                     )} */}
 
-                    {roomType == "voice" ? (
-                        <p className={styles.spacerSmall}></p>
-                    ) : (
-                        <ChatButton
-                            unreadMsg={unreadMsg}
-                            onPress={onToggleChat}
-                        />
-                    )}
-                </header>
-            )}
-        </>
-    );
+          {roomType == "voice" ? (
+            <p className={styles.spacerSmall}></p>
+          ) : (
+            <ChatButton unreadMsg={unreadMsg} onPress={onToggleChat} />
+          )}
+        </header>
+      )}
+    </>
+  );
 };
 
 export default GatherControlBar;
