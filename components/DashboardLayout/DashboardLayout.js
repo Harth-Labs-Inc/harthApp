2a62fb35467d6f3ebb4ee0a7b737e4755ddebe13
Jorge Piquer <piquerjr@gmail.com>
2023-03-31 14:39:01 -0400

    fix lint

--- components/DashboardLayout/DashboardLayout.js
+++ components/DashboardLayout/DashboardLayout.js
@@ -1,6 +1,5 @@
 import { useContext, useState, useEffect } from "react";
 import { MobileContext } from "../../contexts/mobile";
-import dynamic from "next/dynamic";
 import SideNav from "../Menus/SideMenu/SideMenu";
 import TopBar from "../Menus/TopBar/TopBar";
 import MainNav from "../MainNav/MainNav";
@@ -14,125 +13,130 @@ import { useComms } from "contexts/comms";
 import { useSocket } from "contexts/socket";
 
 const DashboardLayout = (props) => {
-  const [menuActive, setmenuActive] = useState(false);
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const { isMobile } = useContext(MobileContext);
+    const [menuActive, setmenuActive] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const { isMobile } = useContext(MobileContext);
 
-  const {
-    changePage,
-    children,
-    currentPage,
-    setShowCreateHarthNameModal,
-    toggleNoHarthDetected,
-  } = props;
+    const {
+        changePage,
+        children,
+        currentPage,
+        setShowCreateHarthNameModal,
+        toggleNoHarthDetected,
+    } = props;
 
-  const { selectedcomm } = useComms();
-  const { getInitialCallRooms, socketID, callRooms } = useVideo();
-  const { user } = useAuth();
-  const { mainAlertsRef, setMainAlertsFromChild } = useSocket();
+    const { selectedcomm } = useComms();
+    const { getInitialCallRooms, socketID, callRooms } = useVideo();
+    const { user } = useAuth();
+    const { mainAlertsRef, setMainAlertsFromChild } = useSocket();
 
-  useEffect(() => {
-    if (socketID && selectedcomm && user) {
-      let creator = selectedcomm.users.find((usr) => usr.userId === user._id);
-      if (creator) {
-        let data = {};
-        data.icon = creator.iconKey;
-        data.name = creator.name;
-        data.harthid = selectedcomm._id;
-        data.socketId = socketID;
-        data.harthName = selectedcomm.name;
-        getInitialCallRooms(data);
-      }
-    }
-  }, [socketID, selectedcomm, user]);
-
-  useEffect(() => {
-    if (selectedcomm) {
-      if (callRooms?.length > 0 && currentPage !== "gather") {
-        let alerts = mainAlertsRef[selectedcomm?._id] || {};
-        if (!alerts.gather || !alerts.gather.hasLive) {
-          alerts.gather = { ...(alerts.gather || {}), hasLive: true };
-          setMainAlertsFromChild({
-            ...mainAlertsRef,
-            [selectedcomm?._id]: alerts,
-          });
+    useEffect(() => {
+        if (socketID && selectedcomm && user) {
+            let creator = selectedcomm.users.find(
+                (usr) => usr.userId === user._id
+            );
+            if (creator) {
+                let data = {};
+                data.icon = creator.iconKey;
+                data.name = creator.name;
+                data.harthid = selectedcomm._id;
+                data.socketId = socketID;
+                data.harthName = selectedcomm.name;
+                getInitialCallRooms(data);
+            }
         }
-      }
-      if (callRooms?.length == 0 && currentPage !== "gather") {
-        let alerts = mainAlertsRef[selectedcomm?._id] || {};
-        if (alerts?.gather?.hasLive) {
-          alerts.gather = { ...alerts?.gather, hasLive: false };
-          setMainAlertsFromChild({
-            ...mainAlertsRef,
-            [selectedcomm?._id]: alerts,
-          });
-        }
-      }
-    }
-  }, [callRooms, mainAlertsRef, selectedcomm]);
+    }, [socketID, selectedcomm, user]);
 
-  const toggleMenu = () => {
+    useEffect(() => {
+        if (selectedcomm) {
+            if (callRooms?.length > 0 && currentPage !== "gather") {
+                let alerts = mainAlertsRef[selectedcomm?._id] || {};
+                if (!alerts.gather || !alerts.gather.hasLive) {
+                    alerts.gather = { ...(alerts.gather || {}), hasLive: true };
+                    setMainAlertsFromChild({
+                        ...mainAlertsRef,
+                        [selectedcomm?._id]: alerts,
+                    });
+                }
+            }
+            if (callRooms?.length == 0 && currentPage !== "gather") {
+                let alerts = mainAlertsRef[selectedcomm?._id] || {};
+                if (alerts?.gather?.hasLive) {
+                    alerts.gather = { ...alerts?.gather, hasLive: false };
+                    setMainAlertsFromChild({
+                        ...mainAlertsRef,
+                        [selectedcomm?._id]: alerts,
+                    });
+                }
+            }
+        }
+    }, [callRooms, mainAlertsRef, selectedcomm]);
+
+    const toggleMenu = () => {
+        if (isMobile) {
+            setMobileMenuOpen((prevState) => !prevState);
+        } else {
+            setmenuActive((prevState) => !prevState);
+        }
+    };
+
     if (isMobile) {
-      setMobileMenuOpen((prevState) => !prevState);
-    } else {
-      setmenuActive((prevState) => !prevState);
+        return (
+            <main className={styles.Dashboard}>
+                <MobileSideNav
+                    mobileMenuOpen={mobileMenuOpen}
+                    onToggleMenu={toggleMenu}
+                    setShowCreateHarthNameModal={setShowCreateHarthNameModal}
+                    changePage={changePage}
+                    toggleNoHarthDetected={toggleNoHarthDetected}
+                />
+
+                <div className={styles.DashboardContent}>
+                    <TopBar
+                        currentPage={currentPage}
+                        changePage={changePage}
+                    ></TopBar>
+                    <section
+                        className={`${styles.DashboardContentWrapper} ${styles.Mobile}`}
+                        id="content_wrapper"
+                    >
+                        {children}
+                    </section>
+                    <MainNav
+                        onToggleMenu={toggleMenu}
+                        changePage={changePage}
+                        currentPage={currentPage}
+                    />
+                </div>
+            </main>
+        );
     }
-  };
 
-  if (isMobile) {
     return (
-      <main className={styles.Dashboard}>
-        <MobileSideNav
-          mobileMenuOpen={mobileMenuOpen}
-          onToggleMenu={toggleMenu}
-          setShowCreateHarthNameModal={setShowCreateHarthNameModal}
-          changePage={changePage}
-          toggleNoHarthDetected={toggleNoHarthDetected}
-        />
-
-        <div className={styles.DashboardContent}>
-          <TopBar currentPage={currentPage} changePage={changePage}></TopBar>
-          <section
-            className={`${styles.DashboardContentWrapper} ${styles.Mobile}`}
-            id="content_wrapper"
-          >
-            {children}
-          </section>
-          <MainNav
-            onToggleMenu={toggleMenu}
-            changePage={changePage}
-            currentPage={currentPage}
-          />
-        </div>
-      </main>
+        <main className={styles.Dashboard}>
+            <SideNav
+                menuOpen={menuActive}
+                onToggleMenu={toggleMenu}
+                setShowCreateHarthNameModal={setShowCreateHarthNameModal}
+                toggleNoHarthDetected={toggleNoHarthDetected}
+            />
+            <div className={styles.DashboardContent}>
+                <TopBar currentPage={currentPage}>
+                    <MainNav
+                        onToggleMenu={toggleMenu}
+                        changePage={changePage}
+                        currentPage={currentPage}
+                    />
+                </TopBar>
+                <section
+                    className={`${styles.DashboardContentWrapper} ${styles.Desktop}`}
+                    id="content_wrapper"
+                >
+                    {children}
+                </section>
+            </div>
+        </main>
     );
-  }
-
-  return (
-    <main className={styles.Dashboard}>
-      <SideNav
-        menuOpen={menuActive}
-        onToggleMenu={toggleMenu}
-        setShowCreateHarthNameModal={setShowCreateHarthNameModal}
-        toggleNoHarthDetected={toggleNoHarthDetected}
-      />
-      <div className={styles.DashboardContent}>
-        <TopBar currentPage={currentPage}>
-          <MainNav
-            onToggleMenu={toggleMenu}
-            changePage={changePage}
-            currentPage={currentPage}
-          />
-        </TopBar>
-        <section
-          className={`${styles.DashboardContentWrapper} ${styles.Desktop}`}
-          id="content_wrapper"
-        >
-          {children}
-        </section>
-      </div>
-    </main>
-  );
 };
 
 export default DashboardLayout;
