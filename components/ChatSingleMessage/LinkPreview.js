fd3a8dbe0a5f679977435772a98657921bc41160
Jorge Piquer <piquerjr@gmail.com>
2023-05-04 19:50:21 -0400

    url updates

--- components/ChatSingleMessage/LinkPreview.js
+++ components/ChatSingleMessage/LinkPreview.js
@@ -5,181 +5,191 @@ import { getURLMetaData } from "../../requests/urls";
 import styles from "./ChatSingleMessage.module.scss";
 
 export const LinkPreview = ({ message }) => {
-  const urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
-  let innerHtml = message;
+    const urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+    let innerHtml = message;
 
-  const [rawURL, setRawURL] = useState();
-  const [alteredURL, setAlteredURL] = useState();
-  const [ogData, setOgData] = useState();
-  const [linkPreview, setLinkPreview] = useState(null);
+    const [rawURL, setRawURL] = useState();
+    const [alteredURL, setAlteredURL] = useState();
+    const [ogData, setOgData] = useState();
+    const [linkPreview, setLinkPreview] = useState(null);
 
-  const wrapLink = (innerHtml, urlRegex) => {
-    let rawurl = "";
-    let replacedURL = innerHtml.replace(urlRegex, function (url) {
-      rawurl = url;
-      return url;
-    });
+    const wrapLink = (innerHtml, urlRegex) => {
+        let rawurl = "";
+        let replacedURL = innerHtml.replace(urlRegex, function (url) {
+            rawurl = url;
+            return url;
+        });
 
-    setRawURL(rawurl);
-    setAlteredURL(replacedURL);
-  };
+        setRawURL(rawurl);
+        setAlteredURL(replacedURL);
+    };
 
-  const getMetaData = async (url) => {
-    const token = localStorage.getItem("token");
-    getURLMetaData(url, token).then(({ data }) => {
-      if (data.ok) {
-        setOgData(data.data);
-      }
-    });
-  };
+    const getMetaData = async (url) => {
+        const token = localStorage.getItem("token");
+        getURLMetaData(url, token).then(({ data }) => {
+            if (data.ok) {
+                setOgData(data.data);
+            }
+        });
+    };
 
-  const getLinkPreview = async (url) => {
-    try {
-      console.log("trying without proxy");
-      const responsewithoutProxy = await axios.get(
-        `https://opengraph.io/api/1.1/extract/${encodeURIComponent(
-          url
-        )}?accept_lang=auto&app_id=bc82985a-b469-4157-8620-76c822cab0c5`
-      );
-      if (
-        responsewithoutProxy.status == 200 &&
-        responsewithoutProxy?.data?.hybridGraph
-      ) {
-        setLinkPreview(responsewithoutProxy.data.hybridGraph);
-      } else {
-        const responsewithProxy = await axios.get(
-          `https://opengraph.io/api/1.1/site/${encodeURIComponent(
-            url
-          )}?accept_lang=auto&use_proxy=true&app_id=bc82985a-b469-4157-8620-76c822cab0c5`
-        );
-        if (
-          responsewithProxy.status == 200 &&
-          responsewithProxy?.data?.hybridGraph
-        ) {
-          setLinkPreview(responsewithProxy.data.hybridGraph);
-        } else {
-          getMetaData(url);
+    const getLinkPreview = async (url) => {
+        try {
+            console.log("trying without proxy");
+            const responsewithoutProxy = await axios.get(
+                `https://opengraph.io/api/1.1/site/${encodeURIComponent(
+                    url
+                )}?accept_lang=auto&app_id=bc82985a-b469-4157-8620-76c822cab0c5`
+            );
+            console.log(responsewithoutProxy, "responsewithoutProxy");
+            if (
+                responsewithoutProxy.status == 200 &&
+                responsewithoutProxy?.data?.hybridGraph
+            ) {
+                setLinkPreview(responsewithoutProxy.data.hybridGraph);
+            } else {
+                const responsewithProxy = await axios.get(
+                    `https://opengraph.io/api/1.1/site/${encodeURIComponent(
+                        url
+                    )}?accept_lang=auto&use_proxy=true&app_id=bc82985a-b469-4157-8620-76c822cab0c5`
+                );
+                if (
+                    responsewithProxy.status == 200 &&
+                    responsewithProxy?.data?.hybridGraph
+                ) {
+                    setLinkPreview(responsewithProxy.data.hybridGraph);
+                } else {
+                    getMetaData(url);
+                }
+            }
+        } catch (error) {
+            console.log("made it");
+            try {
+                console.log("trying with proxy");
+                const responsewithProxy = await axios.get(
+                    `https://opengraph.io/api/1.1/site/${encodeURIComponent(
+                        url
+                    )}?accept_lang=auto&use_proxy=true&app_id=bc82985a-b469-4157-8620-76c822cab0c5`
+                );
+                console.log(responsewithProxy, "responsewithProxy");
+                if (
+                    responsewithProxy.status == 200 &&
+                    responsewithProxy?.data?.hybridGraph
+                ) {
+                    setLinkPreview(responsewithProxy.data.hybridGraph);
+                } else {
+                    console.log("fallboack");
+
+                    getMetaData(url);
+                }
+            } catch (error) {
+                console.log("fallboack");
+
+                getMetaData(url);
+            }
         }
-      }
-    } catch (error) {
-      try {
-        console.log("trying with proxy");
-        const responsewithProxy = await axios.get(
-          `https://opengraph.io/api/1.1/site/${encodeURIComponent(
-            url
-          )}?accept_lang=auto&use_proxy=true&app_id=bc82985a-b469-4157-8620-76c822cab0c5`
-        );
-        console.log(responsewithProxy, "responsewithProxy");
-        if (
-          responsewithProxy.status == 200 &&
-          responsewithProxy?.data?.hybridGraph
-        ) {
-          setLinkPreview(responsewithProxy.data.hybridGraph);
-        } else {
-          console.log("fallboack");
+    };
 
-          getMetaData(url);
+    useEffect(() => {
+        if (urlRegex.test(innerHtml)) {
+            wrapLink(innerHtml, urlRegex);
         }
-      } catch (error) {
-        console.log("fallboack");
+    }, [innerHtml]);
 
-        getMetaData(url);
-      }
+    useEffect(() => {
+        if (rawURL) {
+            getLinkPreview(rawURL);
+        }
+    }, [rawURL]);
+
+    if (
+        linkPreview &&
+        (linkPreview.title ||
+            linkPreview.site_name ||
+            linkPreview.description ||
+            linkPreview.video ||
+            linkPreview.imageSecureUrl ||
+            linkPreview.favicon)
+    ) {
+        return (
+            <>
+                <br />
+                <a
+                    href={rawURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: "none" }}
+                >
+                    <article id="ogCard" className={styles.ogCard}>
+                        <span className={styles.title}>
+                            {linkPreview?.title || linkPreview?.site_name}
+                        </span>
+                        <span>{linkPreview?.description}</span>
+
+                        {linkPreview?.video ? (
+                            <iframe
+                                width="100%"
+                                height="315"
+                                src={linkPreview?.video}
+                                title={linkPreview?.title}
+                                allowFullScreen
+                            />
+                        ) : linkPreview?.image ||
+                          linkPreview?.imageSecureUrl ||
+                          linkPreview?.favicon ? (
+                            <img
+                                src={
+                                    linkPreview?.image ||
+                                    linkPreview?.imageSecureUrl ||
+                                    linkPreview?.favicon
+                                }
+                                alt={linkPreview?.title}
+                                loading="lazy"
+                            />
+                        ) : null}
+                    </article>
+                </a>
+            </>
+        );
     }
-  };
 
-  useEffect(() => {
-    if (urlRegex.test(innerHtml)) {
-      wrapLink(innerHtml, urlRegex);
+    if (ogData) {
+        return (
+            <>
+                <br />
+                <a
+                    href={rawURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: "none" }}
+                >
+                    <article id="ogCard" className={styles.ogCard}>
+                        <span className={styles.title}>
+                            {ogData?.result?.ogTitle ||
+                                ogData?.result?.ogSiteName}
+                        </span>
+                        <span>{ogData?.result?.ogDescription}</span>
+                        <img
+                            src={
+                                ogData?.result?.ogImage?.url ||
+                                ogData?.result?.favicon
+                            }
+                            alt={ogData?.result?.ogTitle}
+                            loading="lazy"
+                        />
+                    </article>
+                </a>
+            </>
+        );
     }
-  }, [innerHtml]);
 
-  useEffect(() => {
     if (rawURL) {
-      getLinkPreview(rawURL);
+        return (
+            <a href={rawURL} target="_blank" rel="noopener noreferrer">
+                {alteredURL}
+            </a>
+        );
     }
-  }, [rawURL]);
 
-  if (
-    linkPreview &&
-    (linkPreview.title ||
-      linkPreview.site_name ||
-      linkPreview.description ||
-      linkPreview.video ||
-      linkPreview.imageSecureUrl ||
-      linkPreview.favicon)
-  ) {
-    return (
-      <>
-        <br />
-        <a
-          href={rawURL}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ textDecoration: "none" }}
-        >
-          <article id="ogCard" className={styles.ogCard}>
-            <span className={styles.title}>
-              {linkPreview?.title || linkPreview?.site_name}
-            </span>
-            <span>{linkPreview?.description}</span>
-
-            {linkPreview?.video ? (
-              <iframe
-                width="100%"
-                height="315"
-                src={linkPreview?.video}
-                title={linkPreview?.title}
-                allowFullScreen
-              />
-            ) : linkPreview?.imageSecureUrl ||
-              linkPreview?.image ||
-              linkPreview?.favicon ? (
-              <img
-                src={linkPreview?.imageSecureUrl || linkPreview?.favicon}
-                alt={linkPreview?.title}
-                loading="lazy"
-              />
-            ) : null}
-          </article>
-        </a>
-      </>
-    );
-  }
-
-  if (ogData) {
-    return (
-      <>
-        <br />
-        <a
-          href={rawURL}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ textDecoration: "none" }}
-        >
-          <article id="ogCard" className={styles.ogCard}>
-            <span className={styles.title}>
-              {ogData?.result?.ogTitle || ogData?.result?.ogSiteName}
-            </span>
-            <span>{ogData?.result?.ogDescription}</span>
-            <img
-              src={ogData?.result?.ogImage?.url || ogData?.result?.favicon}
-              alt={ogData?.result?.ogTitle}
-              loading="lazy"
-            />
-          </article>
-        </a>
-      </>
-    );
-  }
-
-  if (rawURL) {
-    return (
-      <a href={rawURL} target="_blank" rel="noopener noreferrer">
-        {alteredURL}
-      </a>
-    );
-  }
-
-  return null;
+    return null;
 };
