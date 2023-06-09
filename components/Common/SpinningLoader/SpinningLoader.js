44a3ee17a8f6b5bb32cf7400b1223d6cd40d4b01
philjackson91 <philjackson91.pj@gmail.com>
2023-06-09 13:26:17 -0400

    loader initial

---
+++ components/Common/SpinningLoader/SpinningLoader.js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import styles from "./Modal.module.scss";
+import { HarthLogoDark } from "public/images/harth-logo-dark";
+
+export const SpinningLoader = () => {
+  const router = useRouter();
+  const { query } = router;
+
+  const Spinner = () => {
+    return <div className={styles.spinner} />;
+  };
+
+  if (query.gather_window) {
+    return (
+      <div className={`${styles.Maincontainer} ${styles.MaincontainerDark}`}>
+        <div className={`${styles.content}`}>
+          <img height={70} width={70} src={query.harth_icon || ""} />
+          <p style={{ color: "white" }}>{query?.room_name}</p>
+          <Spinner />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className={`${styles.Maincontainer}`}>
+      <div className={`${styles.content}`}>
+        <HarthLogoDark />
+
+        <Spinner />
+      </div>
+    </div>
+  );
+};
