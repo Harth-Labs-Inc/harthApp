0451ff52859219d85f9c40ad325e93db4f14e9da
philjackson91 <philjackson91.pj@gmail.com>
2023-07-05 10:26:24 -0400

    keyboard mobile outside click fix

--- components/Common/Modals/OutsideClick.js
+++ components/Common/Modals/OutsideClick.js
@@ -1,96 +1,69 @@
 import React from "react";
 
 export default class OutsideClickHandler extends React.Component {
-    clickCaptured = false;
+  clickCaptured = false;
+  previousViewportHeight = window.visualViewport.height; // Store the previous viewport height
+  hasResizedSinceClick = false; // Flag to track if resizing has occurred
 
-    render() {
-        if (typeof this.props.children === "function") {
-            return this.props.children(this.getProps());
-        }
-
-        return this.renderComponent();
+  render() {
+    if (typeof this.props.children === "function") {
+      return this.props.children(this.getProps());
     }
 
-    renderComponent() {
-        return React.createElement(
-            this.props.component || "span",
-            { ...this.getProps(), className: this.props.className },
-            this.props.children
-        );
-    }
+    return this.renderComponent();
+  }
 
-    getProps() {
-        return {
-            onMouseDown: this.innerClick,
-            onTouchStart: this.innerClick,
-        };
-    }
+  renderComponent() {
+    return React.createElement(
+      this.props.component || "span",
+      { ...this.getProps(), className: this.props.className },
+      this.props.children
+    );
+  }
 
-    innerClick = () => {
-        this.clickCaptured = true;
+  getProps() {
+    return {
+      onMouseDown: this.innerClick,
+      onTouchStart: this.innerClick,
     };
+  }
 
-    componentDidMount() {
-        document.addEventListener("mousedown", this.documentClick);
-        document.addEventListener("touchstart", this.documentClick);
+  innerClick = (event) => {
+    this.clickCaptured = true;
+    event.stopPropagation(); // Stop event propagation
+  };
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.documentClick);
+    document.addEventListener("touchstart", this.documentClick);
+    window.visualViewport.addEventListener("resize", this.handleViewportResize);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.documentClick);
+    document.removeEventListener("touchstart", this.documentClick);
+    window.visualViewport.removeEventListener(
+      "resize",
+      this.handleViewportResize
+    );
+  }
+
+  documentClick = (event) => {
+    if (
+      !this.hasResizedSinceClick &&
+      !this.clickCaptured &&
+      this.props.onClickOutside
+    ) {
+      this.props.onClickOutside(event);
     }
+    this.clickCaptured = false;
+    setTimeout(() => {
+      this.hasResizedSinceClick = false;
+    }, 100);
+  };
 
-    componentWillUnmount() {
-        document.removeEventListener("mousedown", this.documentClick);
-        document.removeEventListener("touchstart", this.documentClick);
-    }
-
-    documentClick = (event) => {
-        if (!this.clickCaptured && this.props.onClickOutside) {
-            this.props.onClickOutside(event);
-        }
-        this.clickCaptured = false;
-    };
+  handleViewportResize = () => {
+    this.hasResizedSinceClick = true;
+    this.previousViewportHeight = window.visualViewport.height;
+  };
 }
-
-// const OutsideClickHandler = (props) => {
-//   let [clickCaptured, setClickCaptured] = useState(false)
-
-//   useEffect(() => {
-//     document.addEventListener('mousedown', documentClick)
-//     document.addEventListener('touchstart', documentClick)
-//     return () => {
-//       document.removeEventListener('mousedown', documentClick)
-//       document.removeEventListener('touchstart', documentClick)
-//     }
-//   }, [])
-
-//   const documentClick = (event) => {
-//     if (!clickCaptured && props.onClickOutside) {
-//       props.onClickOutside(event)
-//     }
-//     clickCaptured = false
-//   }
-
-//   const innerClick = () => {
-//     setClickCaptured(true)
-//   }
-
-//   const renderComponent = () => {
-//     return React.createElement(
-//       props.component || 'span',
-//       getProps(),
-//       props.children,
-//     )
-//   }
-
-//   const getProps = () => {
-//     return {
-//       onMouseDown: innerClick,
-//       onTouchStart: innerClick,
-//     }
-//   }
-
-//   if (typeof props.children === 'function') {
-//     return props.children(getProps())
-//   }
-
-//   return renderComponent()
-// }
-
-// export default OutsideClickHandler
