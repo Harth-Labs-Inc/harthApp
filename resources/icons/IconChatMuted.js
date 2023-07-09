bf737f4ab5ba16c92d0e05b91fdeb9e4fc3dd433
zilham <bsherwell@gmail.com>
2023-07-09 14:24:01 -0400

    more

--- resources/icons/IconChatMuted.js
+++ resources/icons/IconChatMuted.js
@@ -6,10 +6,10 @@ export const IconChatMuted = () => (
         viewBox={"-3 -4 42 42"}
     >
         <g>
-        <path fill="#b9bfc7" d="M14,30H30l6,6V14a4,4,0,0,0-4-4H14a4,4,0,0,0-4,4V26a4,4,0,0,0,4,4"/>
-        <path fill="#e4e7eb" d="M23,20H6L0,26V4A4,4,0,0,1,4,0H23a4,4,0,0,1,4,4V16a4,4,0,0,1-4,4"/>
-        <rect width="18" height="3" x="4" y="5" fill="#b9bfc7"/>
-        <rect width="14" height="3" x="4" y="11" fill="#b9bfc7"/>
+        <path fill="#48484e" d="M14,30H30l6,6V14a4,4,0,0,0-4-4H14a4,4,0,0,0-4,4V26a4,4,0,0,0,4,4"/>
+        <path fill="#78787e" d="M23,20H6L0,26V4A4,4,0,0,1,4,0H23a4,4,0,0,1,4,4V16a4,4,0,0,1-4,4"/>
+        <rect width="18" height="3" x="4" y="5" fill="#28282e"/>
+        <rect width="14" height="3" x="4" y="11" fill="#28282e"/>
         </g>
     </svg>
 );
