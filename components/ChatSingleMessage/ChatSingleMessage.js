d21dff17912c398ee7943b76f5a056e8d32e1c37
philjackson91 <philjackson91.pj@gmail.com>
2023-07-13 11:52:13 -0400

    added blur back

--- components/ChatSingleMessage/ChatSingleMessage.js
+++ components/ChatSingleMessage/ChatSingleMessage.js
@@ -19,6 +19,26 @@ import { generateID } from "services/helper";
 import { CustomMessageContextMenu } from "components/CustomMessageContextMenu/CustomMessageContextMenu";
 import { EmojiWrapper } from "components/EmojiWrapper/EmojiWrapper";
 
+const shimmer = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#2f1d2a" offset="20%" />
+      <stop stop-color="#282828" offset="50%" />
+      <stop stop-color="#2f1d2a" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str) =>
+  typeof window === "undefined"
+    ? // eslint-disable-next-line
+      Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
 /* eslint-disable */
 const ChatSingleMessage = (props) => {
   const [emojiPickerState, setEmojiPicker] = useState(false);
@@ -410,6 +430,10 @@ const ChatSingleMessage = (props) => {
                       src={urls[idx]}
                       width={desiredWidth || 280}
                       height={desiredHeight || 280}
+                      placeholder="blur"
+                      blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                        shimmer(desiredWidth || 280, desiredHeight || 280)
+                      )}`}
                       alt="message image"
                       onClick={(e) => {
                         e.stopPropagation();
@@ -552,6 +576,10 @@ const ChatSingleMessage = (props) => {
                   src={urls[idx]}
                   width={desiredWidth || 280}
                   height={desiredHeight || 280}
+                  placeholder="blur"
+                  blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                    shimmer(desiredWidth || 280, desiredHeight || 280)
+                  )}`}
                   alt="message image"
                   onClick={(e) => {
                     e.stopPropagation();
