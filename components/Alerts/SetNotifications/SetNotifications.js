899b1da51a10f9be73901e7b5df00c1107953e20
zilham <bsherwell@gmail.com>
2023-08-05 20:46:31 -0400

    more

--- components/Alerts/SetNotifications/SetNotifications.js
+++ components/Alerts/SetNotifications/SetNotifications.js
@@ -3,18 +3,18 @@ import styles from "./SetNotifications.module.scss";
 
 export const SetNotifications = ({request}) => {
   
-  return (
-    <div className={styles.MainContainer}>
-      <div className={styles.title}>
-        Turn on Notifications
-      </div>
+    return (
+        <div className={styles.MainContainer}>
+            <div className={styles.title}>
+                Turn on Notifications
+            </div>
 
-      <div className={styles.subtext}>
-        Enabling notifications will allow you to recieve alerts when your friends post something new
-      </div>
+            <div className={styles.subtext}>
+                Enabling notifications will allow you to recieve alerts when your friends post something new
+            </div>
 
-      <button onClick={request}>Enable Notifications</button>
-      <button className={styles.cancel}>Don't Allow</button>
-    </div>
-  );
+            <button onClick={request}>Enable Notifications</button>
+            <button className={styles.cancel}>Don't Allow</button>
+        </div>
+    );
 };
