7828e139fecbb2351f33b2e104917a3eea4d07eb
philjackson91 <philjackson91.pj@gmail.com>
2023-08-16 14:45:04 -0400

    visibility change / back online reconnect

--- contexts/socket.js
+++ contexts/socket.js
@@ -50,235 +50,278 @@ export const SocketProvider = ({ children }) => {
 
   const mainAlertsRef = useRef({});
 
-  useEffect(() => {
-    if (user) {
-      const token = localStorage.getItem("token");
-      const URLS = socketUrls;
-      const tempSocket = io.connect(URLS[process.env.NODE_ENV], {
-        transports: ["websocket"],
-        query: {
-          token,
-        },
-        reconnection: true,
-        reconnectionAttempts: Infinity,
-        reconnectionDelay: 1000,
-        reconnectionDelayMax: 5000,
-      });
-      tempSocket.on("connect", () => {
-        setSocket(tempSocket);
-        setReconnected((prev) => !prev);
-      });
+  const connectSocket = (user) => {
+    if (!user) return;
 
-      return () => {
-        tempSocket.disconnect();
-        setSocket(null);
-      };
+    if (socketRef.current) {
+      socketRef.current.io.reconnection(false);
+      socketRef.current.disconnect();
     }
-  }, [user]);
 
-  useEffect(() => {
-    if (socket && user) {
-      socketRef.current = socket;
+    const token = localStorage.getItem("token");
+    const URLS = socketUrls;
+    const tempSocket = io.connect(URLS[process.env.NODE_ENV], {
+      transports: ["websocket"],
+      query: { token },
+      reconnection: true,
+      reconnectionAttempts: Infinity,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+    });
 
-      getUnreadMessages(user);
-      getUnreadConvMessages(user);
+    tempSocket.on("connect", () => {
+      socketRef.current = tempSocket;
+      setSocket(tempSocket);
+      setReconnected((prev) => !prev);
+      fetchUnreadData(user);
+      setupListeners(tempSocket, user);
+    });
 
-      socket.on("new update", async ({ updateType, ...incomingUpdate }) => {
-        let activeTopic = JSON.parse(localStorage.getItem("selected_topic"));
-        switch (updateType) {
-          case "new message":
-            if (
-              incomingUpdate.creator_id !== user._id ||
-              incomingUpdate.socketID !== socket.id
-            ) {
-              setIncomingMsg(incomingUpdate);
-              setNewAlerts(incomingUpdate, "chat");
-            }
-            break;
-          case "message update":
-            setIncomingMsgUpdate(incomingUpdate);
+    return tempSocket;
+  };
+
+  const setupListeners = (socket, user) => {
+    if (!socket || !user) return;
+    socket.off("new update");
+    socket.on("new update", async ({ updateType, ...incomingUpdate }) => {
+      let activeTopic = JSON.parse(localStorage.getItem("selected_topic"));
+      switch (updateType) {
+        case "new message":
+          if (
+            incomingUpdate.creator_id !== user._id ||
+            incomingUpdate.socketID !== socket.id
+          ) {
+            setIncomingMsg(incomingUpdate);
             setNewAlerts(incomingUpdate, "chat");
+          }
+          break;
+        case "message update":
+          setIncomingMsgUpdate(incomingUpdate);
+          setNewAlerts(incomingUpdate, "chat");
 
-            break;
-          case "message profile icon update":
-            if (incomingUpdate.userid == user._id && profileRef) {
-              setProfile({
-                ...profileRef,
-                iconKey: incomingUpdate.newIconKey,
-              });
-            }
-            let elements = document.getElementsByClassName(
-              `${incomingUpdate?.harthid}_${incomingUpdate?.userid}`
-            );
-            for (let imgELement of elements) {
-              imgELement.setAttribute(
-                "src",
-                incomingUpdate.newIconKey + "?timestamp=" + new Date().getTime()
-              );
-            }
-            break;
-          case "message profile name update":
-            if (incomingUpdate.userid == user._id && profileRef) {
-              setProfile({
-                ...profileRef,
-                name: incomingUpdate.newName,
-              });
-            }
-            let nameElements = document.getElementsByClassName(
-              `${incomingUpdate?.harthid}_${incomingUpdate?.userid}_name`
-            );
-            for (let nameELement of nameElements) {
-              nameELement.innerHTML = incomingUpdate?.newName;
-            }
-            break;
-          case "new topic":
-            let newTopicResult = await getTopics(
-              incomingUpdate?.comm_id,
-              user._id
-            );
-            if (newTopicResult.topics) {
-              let filteredTopics = newTopicResult.topics.sort((a, b) => {
-                const removeEmoji = (str) => {
-                  return str
-                    .replace(
-                      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-                      ""
-                    )
-                    .replace(/\s+/g, " ")
-                    .trim();
-                };
-                const nameA = removeEmoji(a.title);
-                const nameB = removeEmoji(b.title);
-
-                if (nameA.toLowerCase() < nameB.toLowerCase()) {
-                  return -1;
-                }
-                if (nameA.toLowerCase() > nameB.toLowerCase()) {
-                  return 1;
-                }
-
-                return 0;
-              });
-              setTopics(filteredTopics);
-            }
-
-            break;
-          case "topic deleted":
-            let deleteResult = getTopics(incomingUpdate?.comm?._id, user._id);
-
-            deleteResult.then(({ topics }) => {
-              if (topics) {
-                if (activeTopic?._id === incomingUpdate?.topic?._id) {
-                  setSelectedTopic(topics[0] || {});
-                }
-                setTopics(topics);
-              }
+          break;
+        case "message profile icon update":
+          if (incomingUpdate.userid == user._id && profileRef) {
+            setProfile({
+              ...profileRef,
+              iconKey: incomingUpdate.newIconKey,
             });
-
-            break;
-          case "topic edited":
-            let editResult = await getTopics(
-              incomingUpdate?.comm?._id,
-              user._id
+          }
+          let elements = document.getElementsByClassName(
+            `${incomingUpdate?.harthid}_${incomingUpdate?.userid}`
+          );
+          for (let imgELement of elements) {
+            imgELement.setAttribute(
+              "src",
+              incomingUpdate.newIconKey + "?timestamp=" + new Date().getTime()
             );
-
-            editResult?.topics?.filter(Boolean).forEach((topic) => {
-              if (topic?._id === activeTopic?._id) {
-                localStorage.setItem("selected_topic", JSON.stringify(topic));
-                setSelectedTopic(topic);
-              }
+          }
+          break;
+        case "message profile name update":
+          if (incomingUpdate.userid == user._id && profileRef) {
+            setProfile({
+              ...profileRef,
+              name: incomingUpdate.newName,
             });
-            setTopics(editResult.topics);
-            break;
-          case "harth edited":
-            refetchComms(incomingUpdate?.comm);
-            break;
-          case "harth deleted":
+          }
+          let nameElements = document.getElementsByClassName(
+            `${incomingUpdate?.harthid}_${incomingUpdate?.userid}_name`
+          );
+          for (let nameELement of nameElements) {
+            nameELement.innerHTML = incomingUpdate?.newName;
+          }
+          break;
+        case "new topic":
+          let newTopicResult = await getTopics(
+            incomingUpdate?.comm_id,
+            user._id
+          );
+          if (newTopicResult.topics) {
+            let filteredTopics = newTopicResult.topics.sort((a, b) => {
+              const removeEmoji = (str) => {
+                return str
+                  .replace(
+                    /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+                    ""
+                  )
+                  .replace(/\s+/g, " ")
+                  .trim();
+              };
+              const nameA = removeEmoji(a.title);
+              const nameB = removeEmoji(b.title);
+
+              if (nameA.toLowerCase() < nameB.toLowerCase()) {
+                return -1;
+              }
+              if (nameA.toLowerCase() > nameB.toLowerCase()) {
+                return 1;
+              }
+
+              return 0;
+            });
+            setTopics(filteredTopics);
+          }
+
+          break;
+        case "topic deleted":
+          let deleteResult = getTopics(incomingUpdate?.comm?._id, user._id);
+
+          deleteResult.then(({ topics }) => {
+            if (topics) {
+              if (activeTopic?._id === incomingUpdate?.topic?._id) {
+                setSelectedTopic(topics[0] || {});
+              }
+              setTopics(topics);
+            }
+          });
+
+          break;
+        case "topic edited":
+          let editResult = await getTopics(incomingUpdate?.comm?._id, user._id);
+
+          editResult?.topics?.filter(Boolean).forEach((topic) => {
+            if (topic?._id === activeTopic?._id) {
+              localStorage.setItem("selected_topic", JSON.stringify(topic));
+              setSelectedTopic(topic);
+            }
+          });
+          setTopics(editResult.topics);
+          break;
+        case "harth edited":
+          refetchComms(incomingUpdate?.comm);
+          break;
+        case "harth deleted":
+          refetchComms();
+
+          break;
+        case "selected harth reload":
+          if (incomingUpdate?.comm._id == selectedHarthRef?.current?._id) {
+            setComm(incomingUpdate?.comm);
+          }
+          break;
+        case "selected user reload":
+          if (incomingUpdate?.comm?.selectedUserID == user?._id) {
             refetchComms();
+          }
 
-            break;
-          case "selected harth reload":
-            if (incomingUpdate?.comm._id == selectedHarthRef?.current?._id) {
-              setComm(incomingUpdate?.comm);
-            }
-            break;
-          case "selected user reload":
-            if (incomingUpdate?.comm?.selectedUserID == user?._id) {
-              refetchComms();
-            }
-
-            break;
-          case "new room":
-            setIncomingRoom(incomingUpdate);
-            break;
-          case "room update":
-            setIncomingRoomUpdate(incomingUpdate);
-            break;
-          case "new conversation":
-            let isForYou = incomingUpdate.users?.find(
-              (usr) => usr.userId == user._id
-            );
-            if (isForYou) {
-              setNewAlerts(incomingUpdate, "message");
-              let newConversationsResult = await getConversations(
-                incomingUpdate?.harthId,
-                user._id
-              );
-              setConversations(newConversationsResult.conversations);
-            }
-            break;
-          case "new conversation message":
-            if (incomingUpdate?.creator_type == "Admin") {
-              fetchConversations(
-                incomingUpdate?.harthId ||
-                  incomingUpdate?.comm_id ||
-                  selectedHarthRef.current._id
-              );
-            }
-            if (
-              (incomingUpdate.creator_id !== user._id ||
-                incomingUpdate.socketID !== socket.id) &&
-              incomingUpdate.userIDS?.includes(user._id)
-            ) {
-              setIncomingConversationMessagesHandler(incomingUpdate);
-              setNewAlerts(incomingUpdate, "message");
-            }
-
-            break;
-          case "conversation message update":
-            setIncomingConversationMsgUpdate(incomingUpdate);
+          break;
+        case "new room":
+          setIncomingRoom(incomingUpdate);
+          break;
+        case "room update":
+          setIncomingRoomUpdate(incomingUpdate);
+          break;
+        case "new conversation":
+          let isForYou = incomingUpdate.users?.find(
+            (usr) => usr.userId == user._id
+          );
+          if (isForYou) {
             setNewAlerts(incomingUpdate, "message");
+            let newConversationsResult = await getConversations(
+              incomingUpdate?.harthId,
+              user._id
+            );
+            setConversations(newConversationsResult.conversations);
+          }
+          break;
+        case "new conversation message":
+          if (incomingUpdate?.creator_type == "Admin") {
+            fetchConversations(
+              incomingUpdate?.harthId ||
+                incomingUpdate?.comm_id ||
+                selectedHarthRef.current._id
+            );
+          }
+          if (
+            (incomingUpdate.creator_id !== user._id ||
+              incomingUpdate.socketID !== socket.id) &&
+            incomingUpdate.userIDS?.includes(user._id)
+          ) {
+            setIncomingConversationMessagesHandler(incomingUpdate);
+            setNewAlerts(incomingUpdate, "message");
+          }
 
-            break;
-          case "gather alert":
-            setNewAlerts(incomingUpdate, "gather");
+          break;
+        case "conversation message update":
+          setIncomingConversationMsgUpdate(incomingUpdate);
+          setNewAlerts(incomingUpdate, "message");
 
-            break;
-          case "userLeftConversation":
-            break;
-          case "reload unreads":
+          break;
+        case "gather alert":
+          setNewAlerts(incomingUpdate, "gather");
+
+          break;
+        case "userLeftConversation":
+          break;
+        case "reload unreads":
+          getUnreadMessages(user);
+          break;
+        case "reload same User unreads":
+          if (incomingUpdate.user_id === user._id) {
             getUnreadMessages(user);
-            break;
-          case "reload same User unreads":
-            if (incomingUpdate.user_id === user._id) {
-              getUnreadMessages(user);
-            }
-            break;
-          case "reload conv unreads":
+          }
+          break;
+        case "reload conv unreads":
+          getUnreadConvMessages(user);
+          break;
+        case "reload same User conv unreads":
+          if (incomingUpdate.user_id === user._id) {
             getUnreadConvMessages(user);
-            break;
-          case "reload same User conv unreads":
-            if (incomingUpdate.user_id === user._id) {
-              getUnreadConvMessages(user);
-            }
-            break;
+          }
+          break;
 
-          default:
-            break;
-        }
-      });
+        default:
+          break;
+      }
+    });
+
+    return;
+  };
+
+  const fetchUnreadData = (user) => {
+    getUnreadMessages(user);
+    getUnreadConvMessages(user);
+  };
+
+  useEffect(() => {
+    function handleVisibilityChange() {
+      if (!document.hidden) {
+        manageSocketConnection();
+      }
     }
-  }, [socket?.id, user, reconnected]);
+
+    function handleOffline() {
+      console.log("offline");
+    }
+
+    function handleOnline() {
+      console.log("back online");
+      manageSocketConnection();
+    }
+
+    const manageSocketConnection = () => {
+      if (navigator.onLine && !document.hidden) {
+        connectSocket(user);
+      }
+    };
+
+    manageSocketConnection();
+
+    window.addEventListener("visibilitychange", handleVisibilityChange);
+    window.addEventListener("offline", handleOffline);
+    window.addEventListener("online", handleOnline);
+
+    return () => {
+      window.removeEventListener("visibilitychange", handleVisibilityChange);
+      window.removeEventListener("offline", handleOffline);
+      window.removeEventListener("online", handleOnline);
+
+      if (socket) {
+        socket.disconnect();
+        setSocket(null);
+      }
+    };
+  }, [user]);
 
   useEffect(() => {
     if (selectedcomm) {
@@ -290,7 +333,7 @@ export const SocketProvider = ({ children }) => {
     if (socket?.connected) {
       join([...(comms || [])]);
     }
-  }, [comms, socket?.connected]);
+  }, [comms, socket?.connected, reconnected]);
 
   const setNewAlerts = (incomingUpdate, alertType) => {
     let alerts = { ...mainAlertsRef.current };
