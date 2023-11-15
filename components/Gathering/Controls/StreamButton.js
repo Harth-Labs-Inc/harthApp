12008bffc0f458036c3dcb7fbd638b5b958d83d2
philjackson91 <philjackson91.pj@gmail.com>
2023-11-15 17:55:36 -0500

    hide if cant screen share

--- components/Gathering/Controls/StreamButton.js
+++ components/Gathering/Controls/StreamButton.js
@@ -1,29 +1,41 @@
+import React, { useState, useEffect } from "react";
 import { IconPresentFill } from "../../../resources/icons/IconPresentFill";
 import { IconCancelCastFill } from "../../../resources/icons/IconCancelCastFill";
 import styles from "./gatheringButtons.module.scss";
 
 export const StreamButton = (props) => {
-    const { isMobile = false, onPress, isOn } = props;
+  const { isMobile = false, onPress, isOn } = props;
+  const [canScreenShare, setCanScreenShare] = useState(false);
 
-    return (
-        <button
-            className={`
+  useEffect(() => {
+    if (navigator?.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
+      setCanScreenShare(true);
+    }
+  }, []);
+
+  if (!canScreenShare) {
+    return null;
+  }
+
+  return (
+    <button
+      className={`
                 ${styles.basicButton} 
                 ${isMobile ? styles.basicButtonLarge : styles.basicButtonSmall} 
                 ${isOn ? styles.basicButtonActive : styles.basicButtonInactive}
             `}
-            aria-label="Stream"
-            onClick={onPress}
-        >
-            {isOn ? (
-                <div height="100%" width="100%">
-                    <IconCancelCastFill hasGradient="true" />
-                </div>
-            ) : (
-                <div height="100%" width="100%" className={styles.defaultIcon}>
-                    <IconPresentFill hasGradient={false} />
-                </div>
-            )}
-        </button>
-    );
+      aria-label="Stream"
+      onClick={onPress}
+    >
+      {isOn ? (
+        <div height="100%" width="100%">
+          <IconCancelCastFill hasGradient="true" />
+        </div>
+      ) : (
+        <div height="100%" width="100%" className={styles.defaultIcon}>
+          <IconPresentFill hasGradient={false} />
+        </div>
+      )}
+    </button>
+  );
 };
