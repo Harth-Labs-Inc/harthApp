d5ea0f81401638aeaf896bf4da362a15952583ac
philjackson91 <philjackson91.pj@gmail.com>
2023-12-12 11:12:23 -0500

    should be it

--- services/api.js
+++ services/api.js
@@ -1,16 +1,21 @@
 import Axios from "axios";
-
 import { envUrls } from "../constants/urls";
-import { getBaseUrl } from "./helper";
-
-const URLS = envUrls;
 
 /* eslint-disable */
 
-const dynamicBaseUrl = getBaseUrl();
-const apiUrl = dynamicBaseUrl || URLS.development;
+const getBaseUrl = () => {
+  if (typeof window === "undefined") {
+    if (process.env.IS_QA_ENV === "true") {
+      return envUrls.qa;
+    }
+    return envUrls[process.env.NODE_ENV] || envUrls.development;
+  }
 
-console.log(dynamicBaseUrl, "dynamicBaseUrl");
+  const { protocol, host } = window.location;
+  return `${protocol}//${host}`;
+};
+
+const apiUrl = getBaseUrl();
 
 const api = Axios.create({
   baseURL: apiUrl,
