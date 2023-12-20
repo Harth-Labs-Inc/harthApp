d6cfa115be8f1e35a528a75692462c07f255d57a
philjackson91 <philjackson91.pj@gmail.com>
2023-12-20 15:12:53 -0500

    bored feature

--- pages/dashboard/chat/index.js
+++ pages/dashboard/chat/index.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { useComms } from "../../../contexts/comms";
 import TopicsNav from "../../../components/Menus/TopicsMenu/TopicsSideNav";
 import MobileChatHeader from "../../../components/Topics/MobileChatHeader/MobileChatHeader";
@@ -10,6 +10,64 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 const Chat = () => {
   const { isMobile } = useContext(MobileContext);
   const { selectedTopic, setSelectedTopic } = useComms();
+  const mainChatContainerRef = useRef(null);
+
+  let touchStartX = 0;
+  let touchStartY = 0;
+  let isHorizontalSwipe = null;
+
+  function handleTouchStart(e) {
+    touchStartX = e.touches[0].clientX;
+    touchStartY = e.touches[0].clientY;
+    isHorizontalSwipe = null;
+  }
+
+  function handleTouchMove(e) {
+    const touchCurrentX = e.touches[0].clientX;
+    const touchCurrentY = e.touches[0].clientY;
+    const deltaX = touchCurrentX - touchStartX;
+    const deltaY = touchCurrentY - touchStartY;
+
+    if (isHorizontalSwipe === null) {
+      isHorizontalSwipe = Math.abs(deltaX) > Math.abs(deltaY);
+    }
+
+    if (isHorizontalSwipe && deltaX >= 0 && mainChatContainerRef.current) {
+      mainChatContainerRef.current.style.left = `${deltaX}px`;
+    }
+  }
+
+  function handleTouchEnd(e) {
+    if (isHorizontalSwipe && mainChatContainerRef.current) {
+      const endSwipeX = e.changedTouches[0].clientX;
+      const swipeDistance = endSwipeX - touchStartX;
+
+      if (swipeDistance > 50) {
+        handleMobileChat();
+      } else {
+        mainChatContainerRef.current.style.left = "0px";
+      }
+    }
+
+    isHorizontalSwipe = null;
+  }
+
+  useEffect(() => {
+    const mainChatContainer = mainChatContainerRef.current;
+    if (mainChatContainer) {
+      mainChatContainer.addEventListener("touchstart", handleTouchStart);
+      mainChatContainer.addEventListener("touchmove", handleTouchMove);
+      mainChatContainer.addEventListener("touchend", handleTouchEnd);
+    }
+
+    return () => {
+      if (mainChatContainer) {
+        mainChatContainer.removeEventListener("touchstart", handleTouchStart);
+        mainChatContainer.removeEventListener("touchmove", handleTouchMove);
+        mainChatContainer.removeEventListener("touchend", handleTouchEnd);
+      }
+    };
+  }, [selectedTopic]);
 
   function handleMobileChat() {
     if (Object.keys(selectedTopic || {})?.length && isMobile) {
@@ -36,7 +94,11 @@ const Chat = () => {
               classNames="slide"
             >
               {Object.keys(selectedTopic || {})?.length ? (
-                <div id="mainchatContainer" className={styles.chatHolderMobile}>
+                <div
+                  ref={mainChatContainerRef}
+                  id="mainchatContainer"
+                  className={styles.chatHolderMobile}
+                >
                   <MobileChatHeader
                     selectedTopic={selectedTopic}
                     handleMobileChat={handleMobileChat}
