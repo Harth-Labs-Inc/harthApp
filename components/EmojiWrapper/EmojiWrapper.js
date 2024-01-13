6c422cdc4cbaabd64b5114e1d53a4977c30157d6
Jorge Piquer <piquerjr@gmail.com>
2024-01-13 12:38:16 -0500

    update emoji

--- components/EmojiWrapper/EmojiWrapper.js
+++ components/EmojiWrapper/EmojiWrapper.js
@@ -7,117 +7,125 @@ import Picker from "@emoji-mart/react";
 import { useComms } from "contexts/comms";
 /* eslint-disable */
 export const EmojiWrapper = (props) => {
-  const { addEmoji, closeWrapper } = props;
-  const [emojiData, setEmojiData] = useState([]);
-  const [transition, setTransition] = useState(false);
-  const { isMobile } = useContext(MobileContext);
+    const { addEmoji, closeWrapper } = props;
+    const [emojiData, setEmojiData] = useState([]);
+    const [transition, setTransition] = useState(false);
+    const { isMobile } = useContext(MobileContext);
 
-  const { selectedcomm } = useComms();
+    const { selectedcomm } = useComms();
 
-  useEffect(() => {
-    setTransition(true);
-  }, []);
+    useEffect(() => {
+        setTransition(true);
+    }, []);
 
-  useEffect(() => {
-    if (selectedcomm?._id) {
-      fetchEmojis(selectedcomm?._id);
+    useEffect(() => {
+        if (selectedcomm?._id) {
+            fetchEmojis(selectedcomm?._id);
+        }
+    }, [selectedcomm?._id]);
+
+    function extractIdFromUrl(url) {
+        let shortenedURL = url.split("/").pop().split(".")[0];
+        if (shortenedURL) {
+            return shortenedURL.split("-")[1];
+        }
+        return url;
     }
-  }, [selectedcomm?._id]);
+    const fetchEmojis = async (harthId) => {
+        try {
+            const { ok, urls } = await getCustomEmojis({ harthId });
+            if (ok) {
+                const customEmojis = urls.map((url) => {
+                    return {
+                        id: "custom_" + extractIdFromUrl(url),
+                        name: extractIdFromUrl(url),
+                        keywords: ["custom"],
+                        skins: [{ src: url }],
+                    };
+                });
 
-  function extractIdFromUrl(url) {
-    return url.split("/").pop().split(".")[0];
-  }
-  const fetchEmojis = async (harthId) => {
-    try {
-      const { ok, urls } = await getCustomEmojis({ harthId });
-      if (ok) {
-        const customEmojis = urls.map((url) => ({
-          id: "custom_" + extractIdFromUrl(url),
-          name: extractIdFromUrl(url),
-          keywords: ["custom"],
-          skins: [{ src: url }],
-        }));
-
-        const mergedData = [
-          {
-            id: "custom",
-            name: "Custom",
-            emojis: customEmojis,
-          },
-        ];
-        setEmojiData(mergedData);
-      }
-    } catch (error) {
-      console.error("Error fetching custom emojis:", error);
-    }
-  };
-
-  if (isMobile) {
-    return (
-      <div>
-        <OutsideClickHandler
-          onClickOutside={closeWrapper}
-          onFocusOutside={closeWrapper}
-        >
-          <style jsx global>{`
-            em-emoji-picker {
-              width: 100vw;
-              min-width: 250px;
-              resize: horizontal;
-              overflow: auto;
-              left: 0;
-              position: fixed;
-              z-index: 301;
-              bottom: 0;
-              border-radius: none;
-              transform: translateY(${transition ? "0%" : "100%"});
-              transition: transform 0.2s ease-out;
-              --border-radius: 0px;
+                const mergedData = [
+                    {
+                        id: "custom",
+                        name: "Custom",
+                        emojis: customEmojis,
+                    },
+                ];
+                setEmojiData(mergedData);
             }
-          `}</style>
-          <Picker
-            data={data}
-            custom={emojiData}
-            className={"attach-emoji"}
-            onEmojiSelect={addEmoji}
-            dynamicWidth={true}
-            emojiButtonColors={[
-              "rgba(187, 126, 196, 0.8)",
-              "rgb(13, 161, 181, .8)",
-              "rgba(240, 101, 115, 0.8)",
-              "rgb(0, 163, 150, 0.8)",
-            ]}
-          />
-        </OutsideClickHandler>
-      </div>
-    );
-  }
+        } catch (error) {
+            console.error("Error fetching custom emojis:", error);
+        }
+    };
 
-  return (
-    <div>
-      <OutsideClickHandler
-        onClickOutside={closeWrapper}
-        onFocusOutside={closeWrapper}
-      >
-        <style jsx global>{`
-          em-emoji-picker {
-            z-index: 301;
-          }
-        `}</style>
-        <Picker
-          data={data}
-          custom={emojiData}
-          className="attach-emoji"
-          onEmojiSelect={addEmoji}
-          autoFocus={true}
-          emojiButtonColors={[
-            "rgba(187, 126, 196, 0.8)",
-            "rgb(13, 161, 181, .8)",
-            "rgba(240, 101, 115, 0.8)",
-            "rgb(0, 163, 150, 0.8)",
-          ]}
-        />
-      </OutsideClickHandler>
-    </div>
-  );
+    if (isMobile) {
+        return (
+            <div>
+                <OutsideClickHandler
+                    onClickOutside={closeWrapper}
+                    onFocusOutside={closeWrapper}
+                >
+                    <style jsx global>{`
+                        em-emoji-picker {
+                            width: 100vw;
+                            min-width: 250px;
+                            resize: horizontal;
+                            overflow: auto;
+                            left: 0;
+                            position: fixed;
+                            z-index: 301;
+                            bottom: 0;
+                            border-radius: none;
+                            transform: translateY(
+                                ${transition ? "0%" : "100%"}
+                            );
+                            transition: transform 0.2s ease-out;
+                            --border-radius: 0px;
+                        }
+                    `}</style>
+                    <Picker
+                        data={data}
+                        custom={emojiData}
+                        className={"attach-emoji"}
+                        onEmojiSelect={addEmoji}
+                        dynamicWidth={true}
+                        emojiButtonColors={[
+                            "rgba(187, 126, 196, 0.8)",
+                            "rgb(13, 161, 181, .8)",
+                            "rgba(240, 101, 115, 0.8)",
+                            "rgb(0, 163, 150, 0.8)",
+                        ]}
+                    />
+                </OutsideClickHandler>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <OutsideClickHandler
+                onClickOutside={closeWrapper}
+                onFocusOutside={closeWrapper}
+            >
+                <style jsx global>{`
+                    em-emoji-picker {
+                        z-index: 301;
+                    }
+                `}</style>
+                <Picker
+                    data={data}
+                    custom={emojiData}
+                    className="attach-emoji"
+                    onEmojiSelect={addEmoji}
+                    autoFocus={true}
+                    emojiButtonColors={[
+                        "rgba(187, 126, 196, 0.8)",
+                        "rgb(13, 161, 181, .8)",
+                        "rgba(240, 101, 115, 0.8)",
+                        "rgb(0, 163, 150, 0.8)",
+                    ]}
+                />
+            </OutsideClickHandler>
+        </div>
+    );
 };
--- pages/api/chat/getCustomEmojis.js
+++ pages/api/chat/getCustomEmojis.js
@@ -3,61 +3,58 @@ import getClientWithCheck from "../../../util/getMongoClientWithCheck";
 import { authenticateUser } from "../../../util/authMiddleware";
 const aws = require("aws-sdk");
 aws.config = {
-  accessKeyId: process.env.AWS_ACCESS,
-  secretAccessKey: process.env.AWS_SECRET,
-  region: "us-east-2",
+    accessKeyId: process.env.AWS_ACCESS,
+    secretAccessKey: process.env.AWS_SECRET,
+    region: "us-east-2",
 };
 const s3 = new aws.S3();
 
 /* eslint-disable */
 
 export default async (req, res) => {
-  const authToken = req.headers["x-auth-token"];
-  if (!authToken) {
-    return res.json({ msg: "No token Found", ok: 0, lockDown: true });
-  }
+    const authToken = req.headers["x-auth-token"];
+    if (!authToken) {
+        return res.json({ msg: "No token Found", ok: 0, lockDown: true });
+    }
 
-  let obj;
-  try {
-    obj = JSON.parse(req.body);
-  } catch (e) {
-    obj = req.body;
-  }
+    let obj;
+    try {
+        obj = JSON.parse(req.body);
+    } catch (e) {
+        obj = req.body;
+    }
 
-  const client = await getClientWithCheck(clientPromise);
-  const db = client.db("blarg");
-  const user = await authenticateUser(db, authToken);
+    const client = await getClientWithCheck(clientPromise);
+    const db = client.db("blarg");
+    const user = await authenticateUser(db, authToken);
 
-  if (!user) {
-    return res.status(401).json({ msg: "bad auth", ok: 0, lockDown: true });
-  }
+    if (!user) {
+        return res.status(401).json({ msg: "bad auth", ok: 0, lockDown: true });
+    }
 
-  try {
-    const bucketName = "custom-emoji";
-    const params = {
-      Bucket: bucketName,
-      Prefix: `${obj.harthId}-`,
-    };
+    try {
+        const bucketName = "custom-emoji";
+        const params = {
+            Bucket: bucketName,
+            Prefix: `${obj.harthId}-`,
+        };
 
-    const s3Response = await s3.listObjectsV2(params).promise();
+        const s3Response = await s3.listObjectsV2(params).promise();
 
-    const signedUrls = s3Response.Contents.map((item) => {
-      const url = s3.getSignedUrl("getObject", {
-        Bucket: bucketName,
-        Key: item.Key,
-      });
-      return url;
-    });
+        const signedUrls = s3Response.Contents.map((item) => {
+            const url = `https://custom-emoji.s3.us-east-2.amazonaws.com/${item.Key}`;
+            return url;
+        });
 
-    return res.json({
-      msg: "successful",
-      ok: 1,
-      urls: signedUrls,
-    });
-  } catch (error) {
-    console.error("Error fetching signed URLs from S3:", error);
-    return res
-      .status(500)
-      .json({ msg: "Error fetching signed URLs from S3", ok: 0 });
-  }
+        return res.json({
+            msg: "successful",
+            ok: 1,
+            urls: signedUrls,
+        });
+    } catch (error) {
+        console.error("Error fetching signed URLs from S3:", error);
+        return res
+            .status(500)
+            .json({ msg: "Error fetching signed URLs from S3", ok: 0 });
+    }
 };
