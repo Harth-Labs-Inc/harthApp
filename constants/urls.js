066ba39bca70ff0fa2f15a35079785bb4f76836a
Jorge Piquer <piquerjr@gmail.com>
2024-03-09 15:46:46 -0500

    trying to fix deep link

--- constants/urls.js
+++ constants/urls.js
@@ -1,16 +1,16 @@
 export const envUrls = {
-  development: "http://localhost:3000",
-  qa: "https://qa.hrth.app",
-  production: "https://www.harth.social",
+    development: "http://localhost:3000",
+    qa: "https://qa.hrth.app",
+    production: "https://harth.social",
 };
 
 export const socketUrls = {
-  development: "http://localhost:3030",
-  production: "https://project-blarg-socket.herokuapp.com",
+    development: "http://localhost:3030",
+    production: "https://project-blarg-socket.herokuapp.com",
 };
 
 export const videoSocketUrls = {
-  development: "http://localhost:5030",
-  production: "https://project-blarg-video-socket.herokuapp.com",
-  qa: "https://dev-video-socket-server-dfe43a4d00dc.herokuapp.com",
+    development: "http://localhost:5030",
+    production: "https://project-blarg-video-socket.herokuapp.com",
+    qa: "https://dev-video-socket-server-dfe43a4d00dc.herokuapp.com",
 };
